3dae73fd4fd074e2b18533f04a582b0f2daa39db
AminFiroozi <afiroozi007@gmail.com>
2025-04-02 16:23:54 +0330

    fix(tailwind): setup tailwind usage config

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { vazir } from "@/lib/fonts";
 import { useTheme } from "next-themes";
 import { Toaster as Sonner, ToasterProps } from "sonner";
 
@@ -14,8 +13,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            `group toast group-[.toaster]:bg-background ${vazir.className} group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg`,
-          description: `group-[.toast]:text-muted-foreground`,
+            "group toast group-[.toaster]:bg-background vazir group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+          description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground font-medium",
           cancelButton:
