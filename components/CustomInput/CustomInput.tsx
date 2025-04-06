b4eda6a920939d562f9164d259e574dd42000f81
mr-teikst <erfan.teist2027@gmail.com>
2025-04-06 08:56:13 +0330

    pull develop

--- components/CustomInput/CustomInput.tsx
+++ components/CustomInput/CustomInput.tsx
@@ -4,10 +4,10 @@ import { LucideIcon } from "lucide-react";
 import { useField } from "formik";
 
 interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
-	name: string;
-	children: React.ReactNode;
-	icon?: LucideIcon;
-	onIconClick?: () => void;
+  name: string;
+  children: React.ReactNode;
+  icon?: LucideIcon;
+  onIconClick?: () => void;
   autoFocus?: boolean;
   iconClassName?: string;
   errorClassName?: string;
@@ -16,50 +16,57 @@ interface Props extends React.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const isRTL = (text: string | undefined): boolean => {
-	if (text) {
-		const rtlChars = /[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]/;
-		return rtlChars.test(text);
-	}
-	return true;
+  if (text) {
+    const rtlChars = /[\u0591-\u07FF\uFB1D-\uFDFD\uFE70-\uFEFC]/;
+    return rtlChars.test(text);
+  }
+  return true;
 };
 
 export default function CustomInput({
-	name,
-	children,
-	icon: Icon,
-	onIconClick,
+  name,
+  children,
+  icon: Icon,
+  onIconClick,
   autoFocus = false,
   iconClassName,
   errorClassName,
   inputClassName,
   containerClassName,
-	...props
+  ...props
 }: Props) {
-	// const [isTextRTL, setTextRTL] = useState(true);
-	const [field, meta] = useField(name);
-	const hasError = meta.touched && meta.error;
+  // const [isTextRTL, setTextRTL] = useState(true);
+  const [field, meta] = useField(name);
+  const hasError = meta.touched && meta.error;
 
-	return (
-		<div className={`${style.Conter} ${containerClassName}`}>
-			<div className={style.inputWrapper}>
-				{Icon && <Icon onClick={onIconClick} className={`${style.icon} ${iconClassName}`} />}
-				{field.value === "" && (
-					<label className={`${style.text} ${errorClassName}`}>{children}</label>
-				)}
-				<input
-					dir={isRTL(field.value) ? "rtl" : "ltr"}
-					{...field}
-					{...props}
+  return (
+    <div className={`${style.Conter} ${containerClassName}`}>
+      <div className={style.inputWrapper}>
+        {Icon && (
+          <Icon
+            onClick={onIconClick}
+            className={`${style.icon} ${iconClassName}`}
+          />
+        )}
+        {field.value === "" && (
+          <label className={`${style.text} ${errorClassName}`}>
+            {children}
+          </label>
+        )}
+        <input
+          dir={isRTL(field.value) ? "rtl" : "ltr"}
+          {...field}
+          {...props}
           autoFocus={autoFocus}
-					className={`${style.CustomInput} ${style.numberInput} ${inputClassName} ${
-						isRTL(field.value) ? "text-right" : "text-left"
-					}`}
-					style={{ paddingLeft: Icon ? "42px" : "12px"}}
-				/>
-				{hasError && (
-					<div className={style.errorMessage}>{meta.error}</div>
-				)}
-			</div>
-		</div>
-	);
+          className={`${style.CustomInput} ${
+            style.numberInput
+          } ${inputClassName} ${
+            isRTL(field.value) ? "text-right" : "text-left"
+          }`}
+          style={{ paddingLeft: Icon ? "42px" : "12px" }}
+        />
+        {hasError && <div className={style.errorMessage}>{meta.error}</div>}
+      </div>
+    </div>
+  );
 }
