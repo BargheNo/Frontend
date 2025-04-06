55dfc11f668dfd6eb1adba07964bfd32cedbfea3
AminFiroozi <afiroozi007@gmail.com>
2025-04-07 01:55:42 +0330

    fix(admin-dashboard): complete support UI

--- app/admin-dashboard/support/page.tsx
+++ app/admin-dashboard/support/page.tsx
@@ -1,5 +1,12 @@
+import Tickets from "@/components/admin-dashboard/Tickets/Tickets";
+import FilterSection from "@/components/CorpDashboard/FilterSection";
+import Header from "@/components/Header/Header";
 import React from "react";
 
 export default function page() {
-	return <div></div>;
+	return <div className="page">
+		<Header header="تیکت‌ها" />
+		<FilterSection />
+		<Tickets />
+	</div>;
 }
