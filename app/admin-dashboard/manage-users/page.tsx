c9956ea1e99e3acf6dfb917683eb1888753fe2e6
AminFiroozi <afiroozi007@gmail.com>
2025-04-06 18:26:57 +0330

    feat(admin-dashboard): add filter section

--- app/admin-dashboard/manage-users/page.tsx
+++ app/admin-dashboard/manage-users/page.tsx
@@ -1,9 +1,24 @@
+"use client";
+import FilterSection from '@/components/CorpDashboard/FilterSection'
+import CustomInput from '@/components/Custom/CustomInput/CustomInput'
+import { Formik } from 'formik'
+import { User } from 'lucide-react'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>
-      
+    <div className='flex flex-col items-center gap-12 p-12 w-full'>
+      {/* type of users */}
+      <div className='flex gap-12'>
+        <button className='bg-[#F0EDEF] rounded-2xl px-4 py-2 neu-shadow flex'><p>خریداران</p><User /></button>
+        <button className='bg-[#F0EDEF] rounded-2xl px-4 py-2 neu-shadow flex'><p>فروشندگان</p><User /></button>
+        <button className='bg-[#F0EDEF] rounded-2xl px-4 py-2 neu-shadow flex'><p>متخصصین</p><User /></button>
+        <button className='bg-[#F0EDEF] rounded-2xl px-4 py-2 neu-shadow flex'><p>ادمین‌ها</p><User /></button>
+      </div>
+      {/* filter */}
+      <FilterSection />
+      {/* list */}
+      <div>fji</div>
     </div>
   )
 }
