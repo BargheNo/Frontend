bab4e81868b077a7c73938c33766e8dbe998d3ec
Pooya Tarashi <m.p.t.1383@gmail.com>
2025-04-08 02:29:33 +0330

    feat(repair-panels): fix form style

--- components/Repair/Corp/CorpRepairDialog.tsx
+++ components/Repair/Corp/CorpRepairDialog.tsx
@@ -6,11 +6,13 @@ import {
     DialogHeader,
     DialogTitle,
 } from "@/components/ui/dialog"
-import { useFormik } from 'formik';
+import { Formik } from 'formik';
 import * as Yup from 'yup';
-import { Calendar, SplitSquareVertical } from 'lucide-react';
+import { Calendar, NotebookPen } from 'lucide-react';
+import CustomInput from '@/components/CustomInput/CustomInput';
+import { CustomTextArea } from '@/components/CustomInput/CustomTextArea';
 
-// Define the repair item interface
+
 export interface CorpRepairItem {
     id: string;
     text: string;
@@ -32,22 +34,6 @@ interface CorpRepairDialogProps {
 }
 
 const CorpRepairDialog = ({ isOpen, onClose, repairItem }: CorpRepairDialogProps) => {
-    const formik = useFormik({
-        initialValues: {
-          date: '',
-          title: '',
-          note: '',
-        },
-        validationSchema: Yup.object({
-          date: Yup.string().required('Required'),
-          title: Yup.string().required('Required'),
-          note: Yup.string().required('Required'),
-        }),
-        onSubmit: (values) => {
-          console.log('Form Submitted:', values);
-        },
-    });
-
     if (!repairItem) return null;
 
     return (
@@ -116,115 +102,76 @@ const CorpRepairDialog = ({ isOpen, onClose, repairItem }: CorpRepairDialogProps
                     </div>
 
                     <div className=''>
-                        <form onSubmit={formik.handleSubmit} className="space-y-4">
-                            {/* Date & Title */}
-                            <div className="flex flex-col md:flex-row gap-4">
-                            <div className="flex-1">
-                                <label className="block text-sm font-medium text-gray-700">Date</label>
-                                <input
-                                type="date"
-                                name="date"
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={formik.values.date}
-                                className="w-full inset-input"
-                                />
-                                {formik.touched.date && formik.errors.date && (
-                                <p className="text-red-500 text-xs mt-1">{formik.errors.date}</p>
-                                )}
-                            </div>
+                    <Formik
+                            initialValues={{
+                                date: '',
+                                title: '',
+                                note: '',
+                            }}
+                            validationSchema={Yup.object({
+                                date: Yup.string().required('Required'),
+                                title: Yup.string().required('لطفا عنوان را وارد کنید!'),
+                                note: Yup.string().required('لطفا جزئیات تعمیر را وارد کنید!'),
+                            })}
+                            onSubmit={(values) => {
+                                console.log('Form Submitted:', values);
+                            }}
+                        >
+                            {(formik) => (
+                                <form onSubmit={formik.handleSubmit} className="space-y-4">
+                                    {/* Date & Title */}
+                                    <div className="flex flex-col md:flex-row gap-4">
+                                        <div className="flex-1">
+                                            <input
+                                                type="date"
+                                                name="date"
+                                                onChange={formik.handleChange}
+                                                onBlur={formik.handleBlur}
+                                                value={formik.values.date}
+                                                className="w-full inset-input p-2"
+                                            />
+                                            {formik.touched.date && formik.errors.date && (
+                                                <p className="text-red-500 text-xs mt-1">{formik.errors.date}</p>
+                                            )}
+                                        </div>
 
-                            <div className="flex-1">
-                                <label className="block text-sm font-medium text-gray-700">Title</label>
-                                <input
-                                type="text"
-                                name="title"
-                                placeholder="Note title"
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={formik.values.title}
-                                className="w-full inset-input"
-                                />
-                                {formik.touched.title && formik.errors.title && (
-                                <p className="text-red-500 text-xs mt-1">{formik.errors.title}</p>
-                                )}
-                            </div>
-                            </div>
+                                        <div className="flex-1">
+                                            <CustomInput
+                                                name="title"
+                                                icon={Calendar}
+                                                type="text"
+                                            >
+                                                عنوان
+                                            </CustomInput>
+                                        </div>
+                                    </div>
 
-                            {/* Note */}
-                            <div>
-                            <label className="block text-sm font-medium text-gray-700">Note</label>
-                            <textarea
-                                name="note"
-                                placeholder="Write your note here..."
-                                onChange={formik.handleChange}
-                                onBlur={formik.handleBlur}
-                                value={formik.values.note}
-                                rows={4}
-                                className="w-full inset-input p-2"
-                            />
-                            {formik.touched.note && formik.errors.note && (
-                                <p className="text-red-500 text-xs mt-1">{formik.errors.note}</p>
+                                    {/* Note */}
+                                    <div>
+                                        <CustomTextArea
+                                            name="note"
+                                            icon={NotebookPen}
+                                            textareaClassName="additional-classes-if-needed"
+                                        >
+                                            یادداشت تعمیر
+                                        </CustomTextArea>
+                                    </div>
+
+                                    {/* Submit Button */}
+                                    <div>
+                                        <button
+                                            type="submit"
+                                            className="w-full bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md text-sm font-semibold"
+                                            onClick={() => alert()}
+                                        >
+                                            Submit
+                                        </button>
+                                    </div>
+                                </form>
                             )}
-                            </div>
-
-                            {/* Submit Button */}
-                            <div>
-                            <button
-                                type="submit"
-                                className="w-full bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md text-sm font-semibold"
-                            >
-                                Submit
-                            </button>
-                            </div>
-                        </form>
+                        </Formik>
                     </div>
                 </div>
-
-                {/* <div className="flex flex-col gap-6 p-4">
-                    <div className="flex justify-between items-center">
-                        <h3 className="text-xl font-semibold text-navy-blue">{repairItem.text}</h3>
-                        <span className="text-gray-500">{repairItem.date}</span>
-                    </div>
-                    
-                    <div className="bg-white p-4 rounded-lg shadow-sm">
-                        <h4 className="text-lg font-medium mb-2">اطلاعات پنل</h4>
-                        <p className="text-gray-700">نام پنل: {repairItem.panelName}</p>
-                        <p className="text-gray-700 mt-2">مالک: {repairItem.owner}</p>
-                        
-                        <div className="mt-4 grid grid-cols-3 gap-4">
-                            <div className="flex flex-col">
-                                <span className="text-sm text-gray-500">ظرفیت</span>
-                                <span className="text-lg font-medium">{repairItem.technicalDetails.capacity} کیلووات</span>
-                            </div>
-                            <div className="flex flex-col">
-                                <span className="text-sm text-gray-500">تولید امروز</span>
-                                <span className="text-lg font-medium">{repairItem.technicalDetails.todayProduction} کیلووات</span>
-                            </div>
-                            <div className="flex flex-col">
-                                <span className="text-sm text-gray-500">بازدهی</span>
-                                <span className="text-lg font-medium">{repairItem.technicalDetails.efficiency}%</span>
-                            </div>
-                        </div>
-                    </div>
-                    
-                    <div className="bg-white p-4 rounded-lg shadow-sm">
-                        <h4 className="text-lg font-medium mb-2">آدرس</h4>
-                        <p className="text-gray-700">{repairItem.address}</p>
-                    </div>
-                    
-                    <div className="bg-white p-4 rounded-lg shadow-sm">
-                        <h4 className="text-lg font-medium mb-2">وضعیت تعمیرات</h4>
-                        <div className="flex items-center gap-2">
-                            <div className="w-3 h-3 rounded-full bg-green-500"></div>
-                            <span>در حال انجام</span>
-                        </div>
-                        <p className="mt-2 text-gray-700">
-                            تعمیرکاران ما در حال بررسی و رفع مشکل در پنل شما هستند. 
-                            پیش‌بینی می‌شود این عملیات تا پایان روز جاری به اتمام برسد.
-                        </p>
-                    </div>
-                </div> */}
             </DialogContent>
         </Dialog>
     );
