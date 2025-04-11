0ecf702fafc95f72f08410d02990e23f89ceec40
Pooya Tarashi <m.p.t.1383@gmail.com>
2025-04-11 22:06:58 +0330

    feat(loading-component): change color

--- components/LoadingSpinner/TransparentLoading.tsx
+++ components/LoadingSpinner/TransparentLoading.tsx
@@ -19,7 +19,7 @@ const TransparentLoading: React.FC<TransparentLoadingProps> = ({ className = '',
         <div role="status">
           <svg 
             aria-hidden="true" 
-            className={`${sizeClasses[size]} text-gray-200 animate-spin dark:text-gray-600 fill-blue-600`} 
+            className={`${sizeClasses[size]} text-gray-200 animate-spin dark:text-gray-600 fill-white`} 
             viewBox="0 0 100 101" 
             fill="none" 
             xmlns="http://www.w3.org/2000/svg"
