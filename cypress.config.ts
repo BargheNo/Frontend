47dba5caf72ccbe0f896755b35819a07006b4845
AminFiroozi <afiroozi007@gmail.com>
2025-04-16 18:30:26 +0330

    fix: build errors

--- cypress.config.ts
+++ cypress.config.ts
@@ -8,13 +8,6 @@ export default defineConfig({
 		},
 	},
 
-	component: {
-		devServer: {
-			framework: "next",
-			bundler: "webpack",
-		},
-	},
-
 	e2e: {
 		baseUrl: "http://localhost:3000",
 		setupNodeEvents(on, config) {
