e9bec514b8960e3eed6f6544426ea7df9b2f0361
AminFiroozi <afiroozi007@gmail.com>
2025-04-16 18:04:49 +0330

    ops: setup cypress

---
+++ cypress/e2e/signup.cy.ts
@@ -0,0 +1,18 @@
+describe("Signup Form", () => {
+	it("fill the signup form and show success toast", () => {
+		cy.visit("/signup");
+
+		cy.get('input[name="firstname"]').type("Ali");
+		cy.get('input[name="lastname"]').type("Rezai");
+		cy.get('input[name="phonenumber"]').type("9123456789");
+		cy.get('input[name="password"]').type("Test@1234");
+		cy.get('input[name="confirmpassword"]').type("Test@1234");
+
+		cy.get('input[type="checkbox"]').check({ force: true });
+
+		cy.get("#signup").click();
+		// cy.contains("ثبت نام").click();
+
+		cy.get("#sonner-toast").should("exist").and("contain", "موفق");
+	});
+});
