6cb1f484c886c5fb88b45375ebdf233557cce025
AminFiroozi <afiroozi007@gmail.com>
2025-04-17 12:44:33 +0330

    ops: test push

--- cypress.config.ts
+++ cypress.config.ts
@@ -11,14 +11,14 @@ export default defineConfig({
 	e2e: {
 		baseUrl: "http://localhost:3000",
 		setupNodeEvents(on, config) {
-			// implement node event listeners here
+			
 		},
 	},
-	reporter: "mochawesome",
-	reporterOptions: {
-		reportDir: "cypress/results",
-		overwrite: false,
-		html: false,
-		json: true,
-	},
+	// reporter: "mochawesome",
+	// reporterOptions: {
+	// 	reportDir: "cypress/results",
+	// 	overwrite: false,
+	// 	html: false,
+	// 	json: true,
+	// },
 });
