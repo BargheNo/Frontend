7402fef519820ef1f1c1d2d79d040c52c6d40ce0
Tina.2017 <tina.mpmp2017@gmail.com>
2025-05-02 12:53:09 +0330

    feat:tested the new order page and show the success toast

--- cypress/e2e/newOrder.cy.ts
+++ cypress/e2e/newOrder.cy.ts
@@ -49,4 +49,33 @@ describe("new Order Form", () => {
         cy.get("#newOrderBtn").click();
 		cy.get("#toast-fail").contains("تعداد درخواست های فعال شما از حدنساب عبور کرده است");
 	});
+
+
+
+    it("log in and fill the new Order form and show the success toast", () => {
+        cy.visit("/login");
+
+		cy.get('input[name="phoneNumber"]').type("9113009323");
+		cy.get('input[name="password"]').type("Tina.2017");
+		cy.get("#login").click();
+        cy.wait(3000);
+		cy.visit("/dashboard/new-order");
+        cy.get("#plus").click();
+        cy.get('input[name="name"]').type("new panel for test3");
+        cy.get("#province").click();
+        cy.get("#10").click();
+        cy.get("#city").click();
+        cy.get("#2").click();
+        cy.get('#address').type("خیابان شریعتی جنب شیرینی سرا");
+        cy.get('input[name="code"]').type("2343245345");
+        cy.get('input[name="number"]').type("12");
+        cy.get('input[name="unit"]').type("1");
+        cy.get('input[name="area"]').type("120");
+        cy.get('input[name="electricity"]').type("1200");
+        cy.get('input[name="cost"]').type("120000000");
+        cy.get('#building').click();
+        cy.get("#1").click();
+        cy.get("#newOrderBtn").click();
+		cy.get("#toast-success").contains("درخواست نصب شما با موفقیت ثبت شد");
+	});
 });
