894587ab6a4e8a8b52e38d8319c7e3f906f10878
Tina.2017 <tina.mpmp2017@gmail.com>
2025-05-08 13:38:56 +0330

    feat:added the switch component and customized it

--- components/Messages/message-pagination.tsx
+++ components/Messages/message-pagination.tsx
@@ -25,6 +25,8 @@ import style from "./style.module.css"
 import panelNotFound from "../../public/images/panelNotFound/panelNotFound.png";
 import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
 import SignupButton from "../SignupButton/SignupButton";
+import { Switch } from "@/components/ui/switch"
+
 
 // import { RootState } from "@/src/store/types";
 
@@ -66,16 +68,19 @@ export default function CorpMessagesPagination() {
         <LoadingSpinner />
       ) : history?.length > 0 ? ( */}
       <>
+      
       <h1 className="font-bold text-xl mb-4  md:mr-14 mr-4"> تنظیمات اعلان ها</h1>
       <div className="flex flex-col text-white md:px-14 bg-transparent px-2 w-full">
           <div className="flex flex-col bg-[#F0EDEF] text-gray-800 w-full rounded-2xl overflow-auto shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)]  mb-5">
               <div className="flex flex-row mr-auto ml-30 gap-17">
                     <p className="mt-8">دریافت اعلان از طریق وبسایت</p>
                     <p className="mt-8">دریافت اعلان از طریق ایمیل</p>
+                    
               </div>
               <div className="flex flex-col bg-[#F0EDEF] text-gray-800 w-90/100 rounded-2xl overflow-auto shadow-[inset_-6px_-6px_16px_rgba(255,255,255,0.8),inset_6px_6px_16px_rgba(0,0,0,0.2)] mt-14 m-auto md:h-65 h-60">
                   <div className="flex flex-row justify-between border-b-2 border-gray-300 h-1/3">
                     <p className="mt-auto mb-auto mr-4 text-gray-600">متمایل هستم اعلان ها را به شکل ایمیل دریافت کنم.</p>
+                    
                   </div>
                   <div className="flex flex-row justify-between border-b-2 border-gray-300 h-1/3">
                     <p className="mb-auto mt-auto mr-4 text-gray-600">متمایل هستم اعلان ها را به شکل ایمیل دریافت کنم.</p>
