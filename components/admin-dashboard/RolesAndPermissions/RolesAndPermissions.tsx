168e4eb90288b185ed0284ccca25ffd80377752e
Amirsaeed <amirsaeed7bn@gmail.com>
2025-05-09 22:01:55 +0330

    feature : created the edit role modal and connected the get-permissions-list and get-role apis

--- components/admin-dashboard/RolesAndPermissions/RolesAndPermissions.tsx
+++ components/admin-dashboard/RolesAndPermissions/RolesAndPermissions.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React from "react";
 import styles from "./RolesAndPermissions.module.css";
-import { User, SquareCheckBig, Trash2, Pencil } from "lucide-react";
+import { User, SquareCheckBig, Trash2, Pencil, Plus } from "lucide-react";
 import { useSelector } from "react-redux";
 import generateErrorMessage from "@/src/functions/handleAPIErrors";
 import { toast } from "sonner";
 import { useEffect, useState } from "react";
+import EditRoleModal from "./EditRoleModal";
 
 const RolesAndPermissions = () => {
   type Permission = {
@@ -14,61 +15,73 @@ const RolesAndPermissions = () => {
     description: string;
     category: string;
   };
+  type Role = {
+    id: string;
+    name: string;
+    permissions: Permission[];
+  };
+
   const accessToken = useSelector((state: RootState) => state.user.accessToken);
   const [roles, setRoles] = useState<any[]>([]);
-  
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [currentRole, setCurrentRole] = useState<Role | null>(null);
 
   const getRoles = () => {
-      fetch("http://46.249.99.69:8080/v1/admin/roles", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
+    fetch("http://46.249.99.69:8080/v1/admin/roles", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setRoles(data.data);
       })
-        .then((res) => res.json())
-        .then((data) => {
-          setRoles(data.data);
-        })
-        .catch((err) => {
-          const errMsg =
-            generateErrorMessage(err) || "مشکلی در دریافت نقش ها رخ داد.";
-          toast.error(errMsg);
-        });
-    };
-    const deleteRole = async (roleToDeleteId: string) => {
-        try {
-          const response = await fetch(
-            `http://46.249.99.69:8080/v1/admin/roles/${roleToDeleteId}`,
-            {
-              method: "DELETE",
-              headers: {
-                Authorization: `Bearer ${accessToken}`,
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({}),
-            }
-          );
-    
-          if (!response.ok) {
-            throw new Error("Failed to resolve ticket");
-          }
-    
-          const result = await response.json();
-          toast.success(result.message);
-          getRoles();
-    
-          // Optionally refresh ticket list or update UI
-        } catch (error: any) {
-          const errMsg =
-            generateErrorMessage(error) || "هنگام حذف نقش مشکلی پیش آمد.";
-          toast.error(errMsg);
+      .catch((err) => {
+        const errMsg =
+          generateErrorMessage(err) || "مشکلی در دریافت نقش ها رخ داد.";
+        toast.error(errMsg);
+      });
+  };
+  const deleteRole = async (roleToDeleteId: string) => {
+    try {
+      const response = await fetch(
+        `http://46.249.99.69:8080/v1/admin/roles/${roleToDeleteId}`,
+        {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({}),
         }
-      };
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to resolve ticket");
+      }
+
+      const result = await response.json();
+      toast.success(result.message);
+      getRoles();
+
+      // Optionally refresh ticket list or update UI
+    } catch (error: any) {
+      const errMsg =
+        generateErrorMessage(error) || "هنگام حذف نقش مشکلی پیش آمد.";
+      toast.error(errMsg);
+    }
+  };
+
+  const openEditModal = (role: Role) => {
+    setCurrentRole(role);
+    setIsModalOpen(true);
+  };
 
   useEffect(() => {
-      getRoles();
-    }, []);
+    getRoles();
+  }, []);
 
   const Roles = ({
     id,
@@ -81,7 +94,7 @@ const RolesAndPermissions = () => {
   }) => {
     return (
       <div
-        className={`bg-white p-4 rounded-xl w-full shadow-sm flex items-center gap-3 ltr ${styles.shadow}`}
+        className={`bg-white p-4 rounded-xl w-full shadow-sm flex items-center gap-3 rtl ${styles.shadow}`}
       >
         <div className="flex flex-row justify-between items-center content-center h-full bg-white gap-10 py-5 px-5 overflow-hidden relative border-t-1 border-gray-300 w-full first:border-t-0 min-h-[20px]">
           <div className="flex flex-row gap-2 w-1/3 ">
@@ -92,6 +105,9 @@ const RolesAndPermissions = () => {
             <p className="text-start content-start  text-xl ">{name}</p>
           </div>
           <div className="flex flex-row gap-2 w-5/7 ">
+            <div className="text-orange-500">
+              <SquareCheckBig />
+            </div>
             <p className="content-start w-full text-xl text-gray-600" dir="rtl">
               دسترسی‌ها:{" "}
               {permissions.length === 0
@@ -101,9 +117,6 @@ const RolesAndPermissions = () => {
                     .map((p) => p.description)
                     .join("، ") + (permissions.length > 2 ? "، ..." : "")}
             </p>
-            <div className="text-orange-500">
-              <SquareCheckBig />
-            </div>
           </div>
           <div className="flex flex-row w-1/3 px-3 justify-center ">
             <div
@@ -113,8 +126,8 @@ const RolesAndPermissions = () => {
                 <Pencil />
               </div>
               <button
+                onClick={() => openEditModal({ id, name, permissions })}
                 className="cursor-pointer"
-                // onClick={() => resolverole(id)}
               >
                 تغییر
               </button>
@@ -125,11 +138,7 @@ const RolesAndPermissions = () => {
               <div className="text-orange-500">
                 <Trash2 />
               </div>
-              <button
-                onClick={() => deleteRole(id)}
-                className="cursor-pointer"
-                
-              >
+              <button onClick={() => deleteRole(id)} className="cursor-pointer">
                 حذف
               </button>
             </div>
@@ -143,11 +152,24 @@ const RolesAndPermissions = () => {
       <div
         className={`bg-white p-10 rounded-xl shadow-sm flex flex-col items-center gap-3 w-full justify-center ${styles.outsideShadow}`}
       >
-        <h2
-          className={`text-right text-2xl font-bold text-blue-800 mb-6  pb-4 border-b`}
-        >
-          نقش های فعلی
-        </h2>
+        <div className="flex justify-between items-center w-full mb-6 pb-4 px-10">
+          <h2 className="text-right text-2xl font-bold text-blue-800 border-b">
+            نقش های فعلی
+          </h2>
+          <div
+            className={`cta-neu-button flex flex-row ${styles.button} items-center content-center justify-center h-15 w-1/5 text-2xl `}
+          >
+            <div className="text-orange-500">
+              <Plus />
+            </div>
+            <button
+              className="cursor-pointer"
+              // onClick={() => resolverole(id)}
+            >
+              افزودن نقش
+            </button>
+          </div>
+        </div>
 
         <div className="space-y-6">
           {roles.length > 0 ? (
@@ -164,6 +186,12 @@ const RolesAndPermissions = () => {
           )}
         </div>
       </div>
+      <EditRoleModal
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+        role={currentRole}
+        onSaveSuccess={getRoles}
+      />
     </div>
   );
 };
