35b71cc8a47ced5f1e7a74aba86f91ba0cf22029
Amirsaeed <amirsaeed7bn@gmail.com>
2025-05-11 21:31:19 +0330

    feature : added the userRoles modal and connected the related apis

--- components/admin-dashboard/Users/Users.tsx
+++ components/admin-dashboard/Users/Users.tsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import styles from "./Users.module.css";
 import generateErrorMessage from "@/src/functions/handleAPIErrors";
 import { toast } from "sonner";
+import UserRolesModal from "./UserRoleModal";
 
 type UserType = {
   id: number;
@@ -20,7 +21,61 @@ type RootState = {
   };
 };
 
-const CustomIcon = ({ icon: Icon }: { icon: React.ElementType }) => {
+
+
+export default function Users() {
+  const [users, setUsers] = useState<UserType[]>([]);
+  const [loading, setLoading] = useState(true);
+  const accessToken = useSelector((state: RootState) => state.user.accessToken);
+  const [isRolesModalOpen, setIsRolesModalOpen] = useState(false);
+  const [selectedUserId, setSelectedUserId] = useState<number | null>(null);
+    const [selectedUserStatus, setSelectedUserStatus] = useState<'active' | 'blocked' | null>(null);
+
+
+  const handleManageRoles = (userId: number, status: 'active' | 'blocked') => {
+    setSelectedUserId(userId);
+    setSelectedUserStatus(status);
+    setIsRolesModalOpen(true);
+  };
+
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch(
+        "http://46.249.99.69:8080/v1/admin/users?statuses=1&statuses=2",
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch users");
+      }
+
+      const data = await response.json();
+      setUsers(data.data);
+    } catch (err: any) {
+      const errMsg =
+        generateErrorMessage(err) || "مشکلی در دریافت کاربران رخ داد.";
+      toast.error(errMsg);
+    } finally {
+      setLoading(false);
+    }
+  };
+  useEffect(() => {
+    fetchUsers();
+  }, [accessToken]);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-40">
+        <Loader2 className="animate-spin text-orange-500" size={32} />
+      </div>
+    );
+  }
+
+  const CustomIcon = ({ icon: Icon }: { icon: React.ElementType }) => {
   return (
     <div className={`${styles.icon} bg-[#F4F1F3] text-[#FA682D]`}>
       <Icon className="m-1" />
@@ -28,7 +83,8 @@ const CustomIcon = ({ icon: Icon }: { icon: React.ElementType }) => {
   );
 };
 
-const UserItem = ({ firstName, lastName, phone, status }: UserType) => {
+const UserItem = ({ firstName, lastName, phone, status, id }: UserType) => {
+  const normalizedStatus = status === "block" ? "blocked" : status;
   return (
     <div className="flex flex-row justify-between w-full h-full bg-[#F4F1F3] p-5 overflow-hidden relative border-t-1 border-gray-300 first:border-t-0 items-center">
       <div className="flex items-center gap-3 w-1/4">
@@ -55,6 +111,7 @@ const UserItem = ({ firstName, lastName, phone, status }: UserType) => {
       </div>
       <button
         className={`${styles.button} text-[#FA682D] flex gap-2 items-center p-2 hover:cursor-pointer`}
+       onClick={() => handleManageRoles(id, normalizedStatus)}
       >
         <p className="font-bold">جزئیات بیشتر و مدیریت</p>
         <Settings />
@@ -62,59 +119,6 @@ const UserItem = ({ firstName, lastName, phone, status }: UserType) => {
     </div>
   );
 };
-
-export default function Users() {
-  const [users, setUsers] = useState<UserType[]>([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState<string | null>(null);
-  const accessToken = useSelector((state: RootState) => state.user.accessToken);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const response = await fetch(
-          "http://46.249.99.69:8080/v1/admin/users?statuses=1&statuses=2",
-          {
-            headers: {
-              Authorization: `Bearer ${accessToken}`,
-            },
-          }
-        );
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch users");
-        }
-
-        const data = await response.json();
-        setUsers(data.data);
-      } catch (err: any) {
-        const errMsg =
-          generateErrorMessage(err) || "مشکلی در دریافت کاربران رخ داد.";
-        toast.error(errMsg);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    fetchUsers();
-  }, [accessToken]);
-
-  if (loading) {
-    return (
-      <div className="flex justify-center items-center h-40">
-        <Loader2 className="animate-spin text-orange-500" size={32} />
-      </div>
-    );
-  }
-
-  if (error) {
-    return (
-      <div className="text-red-500 text-center p-4">
-        خطا در دریافت کاربران: {error}
-      </div>
-    );
-  }
-
   return (
     <div className="flex flex-col w-full text-gray-800 rounded-2xl overflow-hidden shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)]">
       {users.map((user) => (
@@ -127,6 +131,13 @@ export default function Users() {
           status={user.status}
         />
       ))}
+      <UserRolesModal
+        isOpen={isRolesModalOpen}
+        onClose={() => setIsRolesModalOpen(false)}
+        userId={selectedUserId || 0}
+        onSaveSuccess={fetchUsers}
+        userStatus={selectedUserStatus}
+      />
     </div>
   );
 }
