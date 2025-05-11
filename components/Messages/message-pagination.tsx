12368a03db82a50fcca5617ccfe18ae993bd4f6f
tinamohammdpour <115643729+tinamohammadpour@users.noreply.github.com>
2025-05-11 20:40:33 +0330

    Feature/complete customer notification page (#159)
    * feat:made the notification setting responsive
    
    * style:decrease the height of the filter bar in mobile device
    
    * fix:made the switch disable for the notif that doesnt support email

--- components/Messages/message-pagination.tsx
+++ components/Messages/message-pagination.tsx
@@ -89,31 +89,62 @@ export default function CorpMessagesPagination() {
       <h1 className="font-bold text-xl mb-4  md:mr-14 mr-4"> تنظیمات اعلان ها</h1>
       <div className="flex flex-col text-white md:px-14 bg-transparent px-2 w-full">
           <div className="flex flex-col bg-[#F0EDEF] text-gray-800 w-full rounded-2xl overflow-auto shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)]  mb-5">
-              <div className="flex flex-row mr-auto ml-30 gap-17">
-                    <p className="mt-8">دریافت اعلان از طریق وبسایت</p>
-                    <p className="mt-8">دریافت اعلان از طریق ایمیل</p>
+              <div className="flex flex-row mr-auto md:ml-30 m-auto md:gap-17 gap-6">
+                    <p className="mt-8 whitespace-nowrap">دریافت از طریق وبسایت</p>
+                    <p className="mt-8 whitespace-nowrap">دریافت از طریق ایمیل</p>
                     
               </div>
-              <div className="flex flex-col bg-[#F0EDEF] text-gray-800 w-90/100 rounded-2xl overflow-auto shadow-[inset_-6px_-6px_16px_rgba(255,255,255,0.8),inset_6px_6px_16px_rgba(0,0,0,0.2)] mt-14 m-auto md:h-65 h-60">
-                  {notifSetting.map((item,index)=>(
-                    <div key={index} className="flex flex-row justify-between border-b-2 border-gray-300 h-1/3">
-                    <p className="mt-auto mb-auto mr-4 text-gray-600">{item.notificationType.name}</p>
-                    <div className="flex flex-row mr-auto ml-30 gap-55 mt-auto mb-auto">
-                      <Switch onClick={()=>{setNameFields(prev =>
-                      prev.map(Item =>
-                        Item.name === item.notificationType.name
-                          ? { ...Item, isPushEnabled: !Item.isPushEnabled }
-                          : Item))}}  disabled={disable} checked={nameFields.find(Item => Item.name === item.notificationType.name)?.isPushEnabled} /> 
-                      <Switch onClick={()=>{setNameFields(prev =>
-                      prev.map(Item =>
-                        Item.name === item.notificationType.name
-                          ? { ...Item, isEmailEnabled: !Item.isEmailEnabled }
-                          : Item))}} disabled={disable} checked={nameFields.find(Item => Item.name === item.notificationType.name)?.isEmailEnabled}/> 
+              <div className="flex flex-col bg-[#F0EDEF] text-gray-800 w-90/100 rounded-2xl  shadow-[inset_-6px_-6px_16px_rgba(255,255,255,0.8),inset_6px_6px_16px_rgba(0,0,0,0.2)] mt-14 m-auto md:h-65 h-60">
+              {notifSetting.length <= 0 ? (
+                <div className="flex flex-1 items-center justify-center h-full">
+                <LoadingSpinner />
+                </div>
+              ) : (
+                notifSetting.map((item, index) => (
+                  <div key={index} className="flex flex-row justify-between border-b-2 border-gray-300 h-1/3 items-center px-4">
+                    <p className="text-gray-600 whitespace-nowrap md:mb-0 mb-9">
+                      {item.notificationType.name}
+                    </p>
+
+                    <div className="flex flex-col">
+                      <div className="flex flex-row md:gap-37 gap-17 md:mt-0 mt-9 md:ml-23">
+                        <Switch
+                          onClick={() => {
+                            setNameFields(prev =>
+                              prev.map(Item =>
+                                Item.name === item.notificationType.name
+                                  ? { ...Item, isPushEnabled: !Item.isPushEnabled }
+                                  : Item
+                              )
+                            );
+                          }}
+                          disabled={disable}
+                          checked={
+                            nameFields.find(Item => Item.name === item.notificationType.name)?.isPushEnabled
+                          }
+                        />
+                        <Switch
+                          onClick={() => {
+                            setNameFields(prev =>
+                              prev.map(Item =>
+                                Item.name === item.notificationType.name
+                                  ? { ...Item, isEmailEnabled: !Item.isEmailEnabled }
+                                  : Item
+                              )
+                            );
+                          }}
+                          disabled={item.notificationType.supportsEmail==false?true:disable}
+                          checked={
+                            nameFields.find(Item => Item.name === item.notificationType.name)?.isEmailEnabled
+                          }
+                        />
+                      </div>
                     </div>
                   </div>
-                  ))}
-                  
+                ))
+              )}
               </div>
+
               <div className="md:w-3/10 w-6/10 mr-auto ml-auto mb-5">
                 <SignupButton onClick={async () => {
                   if (!disable) {
@@ -140,7 +171,7 @@ export default function CorpMessagesPagination() {
 
       <h1 className="font-bold text-xl mb-4  md:mr-14 mr-4">اعلان ها</h1>
         <div className="flex flex-col text-white md:px-14 bg-transparent px-2 w-full">
-            <div className="flex flex-col bg-[#F0EDEF] text-gray-800 w-full rounded-2xl overflow-auto shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)] md:h-20 h-30 mb-5">
+            <div className="flex flex-col bg-[#F0EDEF] text-gray-800 w-full rounded-2xl overflow-auto shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)] h-20 mb-5">
                 <div className={`${style.citypro} flex flex-row mr-4 justify-between m-auto md:w-2/10 w-5/10`}>
                   <Select
                     name="notiftype">
