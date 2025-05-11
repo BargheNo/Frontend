3ae56c661b8f86e3d43b62b37c642f948fff8efd
Amirsaeed <amirsaeed7bn@gmail.com>
2025-05-11 15:30:39 +0330

    fix: fixed the role's name changing senario

--- components/admin-dashboard/RolesAndPermissions/EditRoleModal.tsx
+++ components/admin-dashboard/RolesAndPermissions/EditRoleModal.tsx
@@ -172,9 +172,16 @@ const EditRoleModal: React.FC<EditRoleModalProps> = ({
     <div className={`fixed inset-0 bg-black/60 flex items-center justify-center z-50 p-4 rtl ${vazir.className}`}>
       <div className="bg-white rounded-xl p-6 w-full max-w-4xl max-h-[80vh] overflow-y-auto">
         <div className="flex justify-between items-center mb-4">
-          <h3 className="text-xl font-bold text-blue-800">
-            ویرایش دسترسی‌های نقش: {role.name}
-          </h3>
+            <h3 className="text-xl font-bold text-blue-800">
+              ویرایش دسترسی‌های نقش:
+            </h3>
+            <input
+              type="text"
+              value={roleName}
+              onChange={(e) => setRoleName(e.target.value)}
+              placeholder={role.name}
+              className="p-1 border-b border-blue-800 focus:outline-none focus:border-orange-500 text-lg font-bold text-blue-800 w-30"
+            />
           <button
             onClick={onClose}
             className="text-gray-500 hover:text-gray-700"
