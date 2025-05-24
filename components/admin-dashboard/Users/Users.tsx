c5c1d5b85658174af2c60b2236376f01b881d7cb
AminFiroozi <afiroozi007@gmail.com>
2025-05-24 17:53:19 +0330

    fix: build errors

--- components/admin-dashboard/Users/Users.tsx
+++ components/admin-dashboard/Users/Users.tsx
@@ -173,42 +173,42 @@ export default function Users() {
   );
 };
 
-const UserItem = ({ firstName, lastName, phone, status, id }: UserType) => {
-  const normalizedStatus = status === "block" ? "blocked" : status;
-  return (
-    <div className="flex flex-row justify-between w-full h-full bg-[#F0EDEF] p-5 overflow-hidden relative border-t-1 border-gray-300 first:border-t-0 items-center">
-      <div className="flex items-center gap-3 w-1/4">
-        <CustomIcon icon={User} />
-        <p>
-          {firstName} {lastName}
-        </p>
-      </div>
-      <div className="flex items-center gap-3 w-1/4">
-        <CustomIcon icon={Phone} />
-        <p>{phone.slice(-10)}</p>
-      </div>
-      <div className="flex items-center gap-3 w-1/4">
-        <div className="flex items-center gap-2">
-          <span className="font-bold">
-            {status === "active" ? "فعال" : "مسدود"}
-          </span>
-          <div
-            className={`h-4 w-4 rounded-full ${
-              status === "active" ? "bg-green-500" : "bg-red-500"
-            } shadow-md`}
-          />
-        </div>
-      </div>
-      <button
-        className={`${styles.button} text-[#FA682D] flex gap-2 items-center p-2 hover:cursor-pointer`}
-       onClick={() => handleManageRoles(id, normalizedStatus)}
-      >
-        <p className="font-bold">جزئیات بیشتر و مدیریت</p>
-        <Settings />
-      </button>
-    </div>
-  );
-};
+// const UserItem = ({ firstName, lastName, phone, status, id }: UserType) => {
+//   const normalizedStatus = status === "block" ? "blocked" : status;
+//   return (
+//     <div className="flex flex-row justify-between w-full h-full bg-[#F0EDEF] p-5 overflow-hidden relative border-t-1 border-gray-300 first:border-t-0 items-center">
+//       <div className="flex items-center gap-3 w-1/4">
+//         <CustomIcon icon={User} />
+//         <p>
+//           {firstName} {lastName}
+//         </p>
+//       </div>
+//       <div className="flex items-center gap-3 w-1/4">
+//         <CustomIcon icon={Phone} />
+//         <p>{phone.slice(-10)}</p>
+//       </div>
+//       <div className="flex items-center gap-3 w-1/4">
+//         <div className="flex items-center gap-2">
+//           <span className="font-bold">
+//             {status === "active" ? "فعال" : "مسدود"}
+//           </span>
+//           <div
+//             className={`h-4 w-4 rounded-full ${
+//               status === "active" ? "bg-green-500" : "bg-red-500"
+//             } shadow-md`}
+//           />
+//         </div>
+//       </div>
+//       <button
+//         className={`${styles.button} text-[#FA682D] flex gap-2 items-center p-2 hover:cursor-pointer`}
+//        onClick={() => handleManageRoles(id, normalizedStatus)}
+//       >
+//         <p className="font-bold">جزئیات بیشتر و مدیریت</p>
+//         <Settings />
+//       </button>
+//     </div>
+//   );
+// };
   return (
     <div className="flex flex-col w-full text-gray-800 rounded-2xl overflow-hidden shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)]">
       <FilterUsers 
