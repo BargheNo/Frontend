297cc0d639fe1405e6e2e20c62c3af7dac4d16cd
Pooya Tarashi <m.p.t.1383@gmail.com>
2025-05-27 03:08:05 +0330

    feat(design): change style of IconWithBackground

--- components/CorpDashboard/Warranties/WarrantyCard.tsx
+++ components/CorpDashboard/Warranties/WarrantyCard.tsx
@@ -1,9 +1,15 @@
+import IconWithBackground from '@/components/IconWithBackground/IconWithBackground'
 import React from 'react'
+import { Wrench } from 'lucide-react'
 
 const WarrantyCard = ({name}) => {
   return (
     <div>
         { name }
+        <IconWithBackground
+            icon={Wrench}
+            
+        />
     </div>
   )
 }
