ea0e6fe603e8b6edb0cac29bd73724f4e7ea1975
erfan.teikst <erfan.teikst2027@gmail.com>
2025-06-06 18:12:54 +0330

    feat: add slide2 in landing page

--- components/Landing/BargheNoLanding/BargheNoLanding.tsx
+++ components/Landing/BargheNoLanding/BargheNoLanding.tsx
@@ -1,9 +1,10 @@
+"use client";
 import { ArrowLeft } from "lucide-react";
 import Image from "next/image";
 import { vazir } from "@/lib/fonts";
 import panel from "@/public/images/Landing/panel.png";
-import Link from "next/link";
-import React from "react";
+import Link, { use } from "next/link";
+import React, { useEffect, useState } from "react";
 import styles from "./BargheNoLanding.module.css";
 import { cn } from "@/lib/utils";
 import { useRef } from "react";
@@ -19,10 +20,37 @@ import {
   PerspectiveCamera,
   Stars,
 } from "@react-three/drei";
+import PanelBluePrint from "@/public/images/panels/panel-blueprint-2.jpeg";
+import CleanEnergy from "@/public/images/panels/clean-energy.jpeg";
+import TypewriterComponent from "typewriter-effect";
+import { delay } from "cypress/types/bluebird";
 gsap.registerPlugin(useGSAP);
 gsap.registerPlugin(ScrollTrigger);
 
 export default function BargheNoLanding() {
+  const [position, setPosition] = useState([0, -0.7, 0]);
+  const [rotation, setRotation] = useState([0.1, 1, 0]);
+  const [scale, setScale] = useState(1.4);
+  const [D3PanelRef, setD3PanelRef] = useState<any>(null);
+  const slide1Trigger = {
+    trigger: "#slide-panel1",
+    start: "top top",
+    end: () => `+=${1 * 1000}vh`,
+    toggleActions: "play none none reverse",
+  };
+  const slide2Trigger = {
+    trigger: "#slide-panel2",
+    start: "110% top",
+    end: () => `+=${1 * 1000}vh`,
+    toggleActions: "play none none reverse",
+    markers: true,
+  };
+  const slide3Trigger = {
+    trigger: "#slide-panel3",
+    start: "top top",
+    end: () => `+=${1 * 1000}vh`,
+    toggleActions: "play none none reverse",
+  };
   useGSAP(() => {
     gsap.from(".panel-image", {
       opacity: 0,
@@ -59,7 +87,7 @@ export default function BargheNoLanding() {
         duration: 0.5,
         ease: "power1.inOut",
 
-        markers: true,
+        // markers: true,
       },
     };
 
@@ -70,25 +98,77 @@ export default function BargheNoLanding() {
     });
 
     gsap.to(".D3Panel", {
-      left: "10vw",
-      width: "20vw",
-      top: "10vh",
-      bottom: "50vh",
+      y: "-=20vh",
+      x: "-=2vw",
+      // scale: 0.8,
+      duration: 0.5,
       scrollTrigger: {
         trigger: "#slide-panel1",
-        start: "top top",
+        start: "12% top",
+        // end: "center top",
         end: () => `+=${1 * 1000}vh`,
-        markers: true,
+        // markers: true,
         toggleActions: "play none none reverse",
       },
     });
+    gsap.to("#slide1-text", {
+      opacity: 0,
+      duration: 0.5,
+      scrollTrigger: slide1Trigger,
+    });
+
+    gsap.from("#blueprint", {
+      opacity: 0,
+      y: "+=100%",
+      animation: "power1.inOut",
+      duration: 1,
+      delay: 1.5,
+      repeatDelay: 1.5,
+      scrollTrigger: slide1Trigger,
+    });
+    gsap.from("#clean-card", {
+      opacity: 0,
+      y: "+=100%",
+      animation: "power1.inOut",
+      duration: 1,
+      delay: 1.5,
+      repeatDelay: 1.5,
+      scrollTrigger: slide1Trigger,
+    });
+    // gsap.to("#blueprint", {
+    //   opacity: 0,
+    //   y: "+=100%",
+    //   animation: "power1.inOut",
+    //   duration: 1,
+    //   scrollTrigger: slide2Trigger,
+    // });
+
+    console.log("useGsap runs");
   });
+  useEffect(() => {
+    gsap.to(D3PanelRef?.rotation, {
+      x: 0,
+      y: 3.14,
+      z: 0,
+      duration: 0.5,
+      ease: "power1.inOut",
+      scrollTrigger: slide1Trigger,
+    });
+    gsap.to(D3PanelRef?.scale, {
+      x: 1.1,
+      y: 1.1,
+      z: 1.1,
+      duration: 0.5,
+      ease: "power1.inOut",
+      scrollTrigger: slide1Trigger,
+    });
+  }, [D3PanelRef]);
 
   return (
     <>
-      <div className="container-box h-[100vh]! flex flex-row-reverse max-w-none! w-auto! overflow-x-hidden">
+      <div className="container-box h-[100vh]! flex flex-row-reverse max-w-none! w-auto! overflow-hidden">
         {/* <div className="fixed bg-red-500 top-20! bottom-20! left-20! right-20! w-[60vw] h-[50vh] flex justify-center items-center"> */}
-        <div className="D3Panel fixed z-20 w-[80vw] bottom-[10vh] left-[10vw] top-[35vh] flex items-center justify-center">
+        <div className="D3Panel fixed z-20 w-[40vw] left-[8vw] bottom-[20vh] top-[20vh] flex items-center justify-center rounded-2xl">
           <Canvas
             style={{
               width: "100%",
@@ -97,13 +177,6 @@ export default function BargheNoLanding() {
           >
             <PerspectiveCamera makeDefault position={[0, 1.5, 3]} />
             <OrbitControls />
-            {/* <Stars
-              saturation={0}
-              count={10000}
-              depth={20}
-              // color="#a0a0ff" // Soft blue stars
-              // color2="#ffa0a0" // Secondary pinkish color for variation
-            /> */}
             <ambientLight intensity={0.5} />
             <directionalLight
               position={[10, 10, 5]}
@@ -114,56 +187,113 @@ export default function BargheNoLanding() {
             />
             <pointLight position={[-10, -10, -10]} intensity={0.5} />
             <Environment preset="city" />
-            <D3Panel />
+            <D3Panel
+              position={position}
+              rotation={rotation}
+              scale={scale}
+              ref={setD3PanelRef}
+            />
           </Canvas>
         </div>
-        {/* </div> */}
         <div
           className="w-[100vw] h-[100vh] pt-20 flex-none px-10 gap-2 slide-panel"
           id="slide-panel1"
         >
-          <div className="w-full flex flex-col justify-center items-center gap-20">
-            <div className="flex flex-col items-center justify-center gap-2">
-              <h1
-                className={cn(
-                  `text-9xl font-bold text-[#193947] ${vazir.className} flex place-self-center my-4`,
-                  "h-text"
-                )}
-              >
-                برق نو
-              </h1>
-              <span
-                className={cn(
-                  `${vazir.className} font-bold text-xl text-center`,
-                  "h-text"
-                )}
-              >
-                سامانه جامع مدیریت برق خورشیدی
-              </span>
-            </div>
-            <button
-              className={cn(
-                "bg-fire-orange w-3/5 rounded-full flex items-center justify-center text-white py-3 gap-2 font-bold neo-btn hover:bg-black",
-                "btn-animation"
-              )}
+          <div className="w-full h-full flex flex-row-reverse items-center justify-between">
+            <div className="w-full h-full"></div>
+            <div
+              className="w-full flex flex-col justify-center items-center gap-20"
+              id="slide1-text"
             >
-              <span className={`${vazir.className} text-2xl`}>ورود</span>
-              <ArrowLeft />
-            </button>
+              <div className="flex flex-col items-center justify-center gap-2">
+                <h1
+                  className={cn(
+                    `text-9xl font-bold text-[#193947] ${vazir.className} flex place-self-center my-4`,
+                    "h-text"
+                  )}
+                >
+                  برق نو
+                </h1>
+                <span
+                  className={cn(
+                    `${vazir.className} font-bold text-xl text-center`,
+                    "h-text"
+                  )}
+                >
+                  سامانه جامع مدیریت برق خورشیدی
+                </span>
+              </div>
+              <button
+                className={cn(
+                  "bg-fire-orange w-3/5 rounded-full flex items-center justify-center text-white py-3 gap-2 font-bold hover:bg-black",
+                  "btn-animation"
+                )}
+              >
+                <span className={`${vazir.className} text-2xl`}>ورود</span>
+                <ArrowLeft />
+              </button>
+            </div>
           </div>
         </div>
         <div
-          className="w-[100vw] max-w-none h-[100vh] flex-none slide-panel"
+          className="w-[100vw] max-w-none h-[100vh] flex-none slide-panel pt-[4.2rem] px-3 pb-5"
           id="slide-panel2"
         >
-          hello
+          <div className="flex flex-row justify-between w-full h-full">
+            <div className="w-full h-full p-5 justify-center">
+              <div
+                className="neo-card w-full h-full p-6 rounded-lg bg-warm-white flex flex-col items-center justify-between"
+                id="clean-card"
+              >
+                <span className="self-start text-3xl font-bold">چرا ما؟</span>
+                <div className="w-full h-1/2 mt-4 text-lg text-gray-800">
+                  <TypewriterComponent
+                    options={{ delay: 40 }}
+                    onInit={(typewriter) => {
+                      typewriter
+                        .typeString(
+                          `پنل‌های خورشیدی یکی از بهترین راه‌حل‌های تولید انرژی پاک و
+                  مقرون‌به‌صرفه هستند که با جذب نور خورشید، برق مورد نیاز منازل،
+                  صنایع و مؤسسات را تأمین می‌کنند. کیفیت این پنل‌ها نقش
+                  تعیین‌کننده‌ای در بازدهی و طول عمر سیستم خورشیدی دارد؛ به‌همین
+                  دلیل، محصولات باکیفیت همراه با گارانتی عملکرد ۲۵ ساله ارائه
+                  می‌شوند تا مشتریان با اطمینان کامل از بازدهی مطلوب و دوام بالا
+                  بهره‌مند شوند. نصب اصولی پنل‌های خورشیدی نیز از اهمیت ویژه‌ای
+                  برخوردار است و باید توسط تیم‌های متخصص و دارای مجوز انجام شود
+                  تا از بیشترین جذب انرژی و ایمنی سیستم اطمینان حاصل گردد. با
+                  انتخاب پنل‌های استاندارد و نصب حرفه‌ای، نه‌تنها در هزینه‌های
+                  بلندمدت صرفه‌جویی می‌شود، بلکه سهم ارزشمندی در حفظ محیط زیست و
+                  کاهش مصرف سوخت‌های فسیلی ایفا می‌کنید.`
+                        )
+                        .start();
+                    }}
+                  />
+                </div>
+                <div className="relative h-1/2 aspect-square rounded-2xl overflow-hidden">
+                  <Image src={CleanEnergy} alt="CleanEnergy" fill />
+                </div>
+              </div>
+            </div>
+            <div className=" w-full h-full p-5">
+              <div
+                className="w-full h-full p-3 bg-warm-white neo-card rounded-lg"
+                id="blueprint"
+              >
+                <div className="w-full h-full neo-card-rev p-3 bg-warm-white rounded-lg flex flex-col justify-between ">
+                  <div className="w-full h-full"></div>
+                  <div className="w-full h-full relative rounded-lg overflow-hidden">
+                    <Image src={PanelBluePrint} alt="panel-blue-print" fill />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div
+        {/* <div
           className="w-[100vw] max-w-none h-[100vh] flex-none slide-panel"
           id="slide-panel3"
         >
-          hello
-        </div>
+        </div> */}
       </div>
     </>
     // <div className="w-[90vw] h-screen flex place-self-center items-center">
--- components/Landing/D3Panel/D3Panel.tsx
+++ components/Landing/D3Panel/D3Panel.tsx
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import React, { JSX } from "react";
 import { GLTF } from "three-stdlib";
 import { useGLTF } from "@react-three/drei";
+import { Vector3 } from "@react-three/fiber";
 
 type GLTFResult = GLTF & {
   nodes: {
@@ -13,21 +14,34 @@ type GLTFResult = GLTF & {
   };
 };
 
-export function D3Panel(props: JSX.IntrinsicElements["group"]) {
+export function D3Panel({
+  rotation = [0.1, 1, 0],
+  position = [0, -0.7, 0],
+  scale = 1.4,
+  ref,
+  ...props
+}: {
+  rotation?: any;
+  position?: any;
+  scale?: number;
+  ref?: any;
+  props?: JSX.IntrinsicElements["group"];
+}) {
   const { nodes, materials } = useGLTF(
     "/models/D3Panel/scene.gltf"
   ) as unknown as GLTFResult;
   return (
     <group {...props} dispose={null}>
       <mesh
+        ref={ref}
         castShadow
         receiveShadow
         geometry={nodes.Object_4.geometry}
         material={materials["Material.002"]}
         // rotation={[Math.PI/4, Math.PI / 2.5, 0]}
-        rotation={[0.1,1, 0]}
-              position={[0, -0.7, 0]}
-              scale={1.4}
+        rotation={rotation}
+        position={position}
+        scale={scale}
       />
     </group>
   );
