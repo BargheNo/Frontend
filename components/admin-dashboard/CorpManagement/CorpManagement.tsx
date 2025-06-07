a41e718717ac9c39e472efdcc97cb8cfbbe018ab
Amirsaeed <amirsaeed7bn@gmail.com>
2025-06-08 00:53:37 +0330

    feat : initiated the corp management page

---
+++ components/admin-dashboard/CorpManagement/CorpManagement.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CorpManagement = () => {
+  return (
+    <div className='flex justify-center items-center'>
+      <h1>CORP MANAGEMENT</h1>
+    </div>
+  )
+}
+
+export default CorpManagement
