2fce20777b5af74fe54a12dd783d42e513c6c1d4
AminFiroozi <afiroozi007@gmail.com>
2025-06-08 20:32:43 +0330

    fix: my panels with minimum ui

--- app/dashboard/my-panels/page.tsx
+++ app/dashboard/my-panels/page.tsx
@@ -3,24 +3,87 @@ import PageContainer from "@/components/Dashboard/PageContainer/PageContainer";
 import Header from "@/components/Header/Header";
 import PanelCard from "@/components/Panel/PanelCard/PanelCard";
 import { getData } from "@/src/services/apiHub";
+import panelNotFound from "@/public/images/panelNotFound/panelNotFound.png";
+import Image from "next/image";
 import { useEffect, useState } from "react";
+import LoadingSpinner from "@/components/Loading/LoadingSpinner/LoadingSpinner";
+
+interface PanelProps {
+	id: number;
+	name: string;
+	status: string;
+	buildingType: string;
+	area: number;
+	power: number;
+	tilt: number;
+	azimuth: number;
+	totalNumberOfModules: number;
+	guaranteeStatus: string;
+	address: Address;
+	corporation: {
+		id: number;
+		name: string;
+		logo: string;
+		addresses: Address[];
+	};
+}
 
 const Settings = () => {
-	const [panels, setPanels] = useState([]);
+	const [loading, setLoading] = useState<boolean>(true);
+	const [panels, setPanels] = useState<PanelProps[]>([]);
 	useEffect(() => {
+		setLoading(true);
 		getData({
 			endPoint: `/v1/user/installation/panel?status=1&offset=5&limit=1`,
-		}).then((data) => {
-			console.log(data);
-			setPanels(data?.data);
-		});
+		})
+			.then((data) => {
+				console.log(data);
+				setPanels(data?.data);
+			})
+			.finally(() => setLoading(false));
 	}, []);
 	return (
 		// <div className="min-h-full flex flex-col text-white py-8 px-14 bg-transparent">
 		<PageContainer>
 			<Header header="پنل‌های من" />
 			<div className="flex flex-col text-gray-800 rounded-2xl overflow-hidden shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)]">
-				<PanelCard
+				{loading ? (
+					<LoadingSpinner />
+				) : panels.length > 0 ? (
+					panels.map((panel: PanelProps, index) => (
+						<PanelCard
+							key={index}
+							id={String(panel?.id)}
+							panelName={panel.name}
+							technicalDetails={{
+								capacity: panel.power,
+								todayProduction: 12.3,
+								efficiency: 92,
+							}}
+              status={panel.status}
+							address={`استان ${panel.address.province}، شهر ${panel.address.city}، ${panel.address.streetAddress}`}
+						/>
+					))
+				) : panels ? (
+					<div className="text-center place-items-center mt-6">
+						<Image
+							className="w-1/3"
+							src={panelNotFound}
+							alt="orderNotFound"
+						/>
+						<div className="-mt-8">
+							<p
+								className=" mt-6 text-navy-blue font-bold rtl"
+								style={{ fontSize: "1.1rem" }}
+							>
+								هیچ پنلی یافت نشد.
+							</p>
+						</div>
+					</div>
+				) : (
+					<></>
+				)}
+				{/* <PanelCard
 					id="8"
 					panelName="پنل باغ 1"
 					technicalDetails={{
@@ -51,7 +114,7 @@ const Settings = () => {
 						efficiency: 92,
 					}}
 					address="باغ تالش"
-				/>
+				/> */}
 			</div>
 		</PageContainer>
 		// </div>
