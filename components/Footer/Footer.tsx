877ef314450dd7c547d062000625e21a9beb86e1
erfan.teikst <erfan.teikst2027@gmail.com>
2025-06-08 12:22:13 +0330

    feat: enhance Footer component with copyright information and styling

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
 export default function Footer() {
-  return <div className="h-40 bg-black w-screen"></div>;
+  return <div className="pb-20 md:pb-4 bg-black w-screen text-white flex flex-col items-center justify-start px-4 pt-4">
+    <p className="text-sm">تمامی حقوق این سایت متعلق به شرکت برق نو می باشد.</p>
+    <p className="text-sm" dir="ltr">© 2024 Barghe No. All rights reserved.</p>
+  </div>;
 }
