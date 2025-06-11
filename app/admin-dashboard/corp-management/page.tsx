6aa5653d5aea531660804ce4550d6014c8a497a7
AminFiroozi <afiroozi007@gmail.com>
2025-06-11 10:21:35 +0330

    fix: right align the statuses select in admin-dashboard/corp-management

--- app/admin-dashboard/corp-management/page.tsx
+++ app/admin-dashboard/corp-management/page.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import CorpManagement from '@/components/admin-dashboard/CorpManagement/CorpManagement'
+import PageContainer from '@/components/Dashboard/PageContainer/PageContainer'
+import Header from '@/components/Header/Header'
 
 const page = () => {
   return (
-    <div>
+    <PageContainer>
+      <Header header='شرکت های فعلی' />
       <CorpManagement/>
-    </div>
+    </PageContainer>
   )
 }
 
