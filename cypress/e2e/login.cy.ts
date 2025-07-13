c0a005d7cafe4e3d63f44e3ca4d7584e97bebd58
AminFiroozi <afiroozi007@gmail.com>
2025-07-13 15:24:06 +0330

    fix: login test

--- cypress/e2e/login.cy.ts
+++ cypress/e2e/login.cy.ts
@@ -5,9 +5,11 @@ describe("Login Form", () => {
 		cy.get('input[name="phoneNumber"]').type("9123456789");
 		cy.get('input[name="password"]').type("Test@1234");
 
-		cy.get("#login").click();
+		cy.get('[data-test="submit"]').click();
 		// cy.contains("ثبت نام").click();
 
-		cy.get("#sonner-toast").should("exist").and("contain", "این کاربر پیدا نشد");
+		cy.get('[data-test="sonner-toast"]')
+			.should("exist")
+			.and("contain", "این کاربر پیدا نشد");
 	});
 });
