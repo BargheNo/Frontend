bc08885003096a7369c715d5d0695eee2c33ae7f
AminFiroozi <afiroozi007@gmail.com>
2025-07-13 12:27:19 +0330

    feat(maintenance): apply maintenance.acceptRequest permission

--- components/Custom/CustomToast/CustomToast.tsx
+++ components/Custom/CustomToast/CustomToast.tsx
@@ -3,13 +3,40 @@ export default function CustomToast(
 	message: string,
 	mode?: "success" | "error" | "warning" | "info" | undefined
 ) {
-	if (mode === "success")
-		return toast.success(<div data-test="sonner-toast">{message}</div>);
+	function Toast(message: string) {
+		return (
+			<div data-test="sonner-toast" className="select-none">
+				{message}
+			</div>
+		);
+	}
+	if (mode === "success") return toast.success(Toast(message));
 	else if (mode === "error")
-		return toast.error(<div data-test="sonner-toast">{message}</div>);
+		return toast.error(
+			Toast(message)
+			// <div data-test="sonner-toast" className="select-none">
+			// 	{message}
+			// </div>
+		);
 	else if (mode === "warning")
-		return toast.warning(<div data-test="sonner-toast">{message}</div>);
+		return toast.warning(
+			Toast(message)
+			// <div data-test="sonner-toast" className="select-none">
+			// 	{message}
+			// </div>
+		);
 	else if (mode === "info")
-		return toast.info(<div data-test="sonner-toast">{message}</div>);
-	else return toast(<div data-test="sonner-toast">{message}</div>);
+		return toast.info(
+			Toast(message)
+			// <div data-test="sonner-toast" className="select-none">
+			// 	{message}
+			// </div>
+		);
+	else
+		return toast(
+			Toast(message)
+			// <div data-test="sonner-toast" className="select-none">
+			// 	{message}
+			// </div>
+		);
 }
--- components/Repair/Corp/CorpRepairDialog.tsx
+++ components/Repair/Corp/CorpRepairDialog.tsx
@@ -12,33 +12,33 @@ import RepairHistory from "./RepairHistory";
 import RepairForm from "./RepairForm";
 import LoadingSpinner from "@/components/Loading/LoadingSpinner/LoadingSpinner";
 import CustomToast from "@/components/Custom/CustomToast/CustomToast";
-import { Button } from '@/components/ui/button';
-import { CheckCircle, XCircle } from 'lucide-react';
+import { Button } from "@/components/ui/button";
+import { CheckCircle, XCircle } from "lucide-react";
+import useHasPermission from "@/src/functions/hasPermission";
 
 interface RepairHistoryProps {
-    
-        id: string;
-        operator: {
-            firstName: string;
-            lastName: string;
-        };
-        createdAt: string;
-        title: string;
-        details: string;
-        violation?: {
-            reason: string;
-            details: string;
-        };
-    
+	id: string;
+	operator: {
+		firstName: string;
+		lastName: string;
+	};
+	createdAt: string;
+	title: string;
+	details: string;
+	violation?: {
+		reason: string;
+		details: string;
+	};
 }
-
-
 
 const CorpRepairDialog = ({
 	isOpen,
 	onClose,
 	repairItem,
 }: CorpRepairDialogProps) => {
+	const hasAcceptMaintenanceRequestPermission = useHasPermission(
+		"maintenance.acceptRequest"
+	);
 	// const [notes, setNotes] = useState<MaintenanceRecord[] | null>(null);
 	const [notes, setNotes] = useState<RepairHistoryProps | null>(null);
 	const [isLoadingNotes, setIsLoadingNotes] = useState(true);
@@ -48,11 +48,11 @@ const CorpRepairDialog = ({
 	useEffect(() => {
 		if (repairItem) {
 			getData({
-				endPoint: `${baseURL}/v1/corp/2/maintenance/request/${repairItem.id}`,    // TODO: corpIDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
+				endPoint: `${baseURL}/v1/corp/2/maintenance/request/${repairItem.id}`, // TODO: corpIDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 			})
 				.then((res) => {
 					setNotes(res.data.record);
-					console.log(res.data.record)
+					console.log(res.data.record);
 					setIsLoadingNotes(false);
 				})
 				.catch(() => {
@@ -67,30 +67,35 @@ const CorpRepairDialog = ({
 	const handleAccept = async () => {
 		setIsLoading(true);
 		putData({
-			endPoint: `${baseURL}/v1/corp/2/maintenance/request/${repairItem.id}/accept`    // TODO: corpIDDDDDDDDDDDDDDDDDDDDDDDDDDDD
-
-		}).then(() => {
-			CustomToast("درخواست با موفقیت تایید شد", "success");
-			onClose();
-		}).catch(() => {
-			CustomToast("خطا در تایید درخواست", "error");
-		}).finally(() => {
-			setIsLoading(false);
+			endPoint: `${baseURL}/v1/corp/2/maintenance/request/${repairItem.id}/accept`, // TODO: corpIDDDDDDDDDDDDDDDDDDDDDDDDDDDD
 		})
+			.then(() => {
+				CustomToast("درخواست با موفقیت تایید شد", "success");
+				onClose();
+			})
+			.catch(() => {
+				CustomToast("خطا در تایید درخواست", "error");
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
 	};
 
 	const handleReject = async () => {
 		setIsLoading(true);
 		putData({
-			endPoint: `${baseURL}/v1/corp/2/maintenance/request/${repairItem.id}/reject`    // TODO: corpIDDDDDDDD
-		}).then(() => {
-			CustomToast("درخواست با موفقیت رد شد", "success");
-			onClose();
-		}).catch(() => {
-			CustomToast("خطا در رد درخواست", "error");
-		}).finally(() => {
-			setIsLoading(false);
+			endPoint: `${baseURL}/v1/corp/2/maintenance/request/${repairItem.id}/reject`, // TODO: corpIDDDDDDDD
 		})
+			.then(() => {
+				CustomToast("درخواست با موفقیت رد شد", "success");
+				onClose();
+			})
+			.catch(() => {
+				CustomToast("خطا در رد درخواست", "error");
+			})
+			.finally(() => {
+				setIsLoading(false);
+			});
 	};
 
 	return (
@@ -134,27 +139,28 @@ const CorpRepairDialog = ({
 
 						{/* Action Buttons */}
 						<div className="flex justify-end gap-4">
-							{repairItem.status === "در انتظار تایید" && (
-								<>
-									<Button
-										variant="outline"
-										className="flex items-center gap-2 text-red-600 hover:text-red-700"
-										onClick={handleReject}
-										disabled={isLoading}
-									>
-										<XCircle size={20} />
-										رد درخواست
-									</Button>
-									<Button
-										className="flex items-center gap-2 bg-green-600 hover:bg-green-700"
-										onClick={handleAccept}
-										disabled={isLoading}
-									>
-										<CheckCircle size={20} />
-										تایید درخواست
-									</Button>
-								</>
-							)}
+							{hasAcceptMaintenanceRequestPermission &&
+								repairItem.status === "در انتظار تایید" && (
+									<>
+										<Button
+											variant="outline"
+											className="flex items-center gap-2 text-red-600 hover:text-red-700"
+											onClick={handleReject}
+											disabled={isLoading}
+										>
+											<XCircle size={20} />
+											رد درخواست
+										</Button>
+										<Button
+											className="flex items-center gap-2 bg-green-600 hover:bg-green-700"
+											onClick={handleAccept}
+											disabled={isLoading}
+										>
+											<CheckCircle size={20} />
+											تایید درخواست
+										</Button>
+									</>
+								)}
 							{repairItem.status === "تایید شده" && (
 								<Button
 									onClick={() => setIsFormOpen(true)}
