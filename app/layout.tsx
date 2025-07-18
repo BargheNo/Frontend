1f9f69ea9f35d4f9d170f6f5e4e295f9234cb903
AminFiroozi <afiroozi007@gmail.com>
2025-07-18 14:30:56 +0330

    fix: delete all scrollbars from site

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="fa">
+    <html lang="fa" className="no-scrollbar">
       <body className="">
         <Providers>
           <NavbarWrapper />
