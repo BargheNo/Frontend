3623cc3f5a55fef88073ca1d2b1ed692c4355aa2
AminFiroozi <afiroozi007@gmail.com>
2025-08-01 15:13:05 +0330

    fix: build errors

--- components/CorpDashboard/FilterSection.tsx
+++ components/CorpDashboard/FilterSection.tsx
@@ -30,111 +30,112 @@ const validationSchema = Yup.object({
 		),
 });
 export default function FilterSection() {
-	const [sorting, setSorting] = useState("most-recent");
-	const [resultPerPage, setResultPerPage] = useState("10");
-	const handleFormSubmit = async ({
-		search,
-		sorting,
-		resultPerPage,
-	}: {
-		search?: string;
-		sorting?: string;
-		resultPerPage?: string;
-	}) => {
-		setSorting(sorting);
-		setResultPerPage(resultPerPage);
-	};
+	// const [sorting, setSorting] = useState("most-recent");
+	// const [resultPerPage, setResultPerPage] = useState("10");
+	// const handleFormSubmit = async ({
+	// 	search,
+	// 	sorting,
+	// 	resultPerPage,
+	// }: {
+	// 	search?: string;
+	// 	sorting?: string;
+	// 	resultPerPage?: string;
+	// }) => {
+	// 	setSorting(sorting);
+	// 	setResultPerPage(resultPerPage);
+	// };
 	return (
-		<div className="flex flex-col relative w-full text-gray-800 rounded-2xl overflow-hidden bg-[#F0EDEF] shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)]">
-			<Formik
-				initialValues={initialValues}
-				validationSchema={validationSchema}
-				onSubmit={handleFormSubmit}
-			>
-				<div className="p-5 items-center flex gap-6">
-					{/* <div className="w-4/5">
-						<CustomInput
-							placeholder="جستجو"
-							name="search"
-							icon={Search}
-							type="text"
-							containerClassName="-translate-y-[11px]"
-							inputClassName="bg-white"
-						/>
-					</div> */}
-					<div className="flex gap-4 items-center">
-						<Select
-							defaultValue="most-recent"
-							value={sorting}
-							onValueChange={(value) => {
-								setSorting(value);
-							}}
-						>
-							<SelectTrigger
-								dir="rtl"
-								className="w-40 cursor-pointer bg-white"
-							>
-								<SelectValue placeholder="جدیدترین" />
-							</SelectTrigger>
-							<SelectContent dir="rtl">
-								<SelectItem
-									value="most-recent"
-									className="cursor-pointer"
-								>
-									جدیدترین
-								</SelectItem>
-								<SelectItem
-									value="top-visited"
-									className="cursor-pointer"
-								>
-									بر اساس وضعیت
-								</SelectItem>
-							</SelectContent>
-						</Select>
+		<div></div>
+		// <div className="flex flex-col relative w-full text-gray-800 rounded-2xl overflow-hidden bg-[#F0EDEF] shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)]">
+		// 	<Formik
+		// 		initialValues={initialValues}
+		// 		validationSchema={validationSchema}
+		// 		onSubmit={handleFormSubmit}
+		// 	>
+		// 		<div className="p-5 items-center flex gap-6">
+		// 			{/* <div className="w-4/5">
+		// 				<CustomInput
+		// 					placeholder="جستجو"
+		// 					name="search"
+		// 					icon={Search}
+		// 					type="text"
+		// 					containerClassName="-translate-y-[11px]"
+		// 					inputClassName="bg-white"
+		// 				/>
+		// 			</div> */}
+		// 			<div className="flex gap-4 items-center">
+		// 				<Select
+		// 					defaultValue="most-recent"
+		// 					value={sorting}
+		// 					onValueChange={(value) => {
+		// 						setSorting(value);
+		// 					}}
+		// 				>
+		// 					<SelectTrigger
+		// 						dir="rtl"
+		// 						className="w-40 cursor-pointer bg-white"
+		// 					>
+		// 						<SelectValue placeholder="جدیدترین" />
+		// 					</SelectTrigger>
+		// 					<SelectContent dir="rtl">
+		// 						<SelectItem
+		// 							value="most-recent"
+		// 							className="cursor-pointer"
+		// 						>
+		// 							جدیدترین
+		// 						</SelectItem>
+		// 						<SelectItem
+		// 							value="top-visited"
+		// 							className="cursor-pointer"
+		// 						>
+		// 							بر اساس وضعیت
+		// 						</SelectItem>
+		// 					</SelectContent>
+		// 				</Select>
 
-						<Select
-							defaultValue={"20"}
-							value={String(resultPerPage)}
-							onValueChange={(value) => {
-								setResultPerPage(value);
-							}}
-						>
-							<SelectTrigger
-								dir="rtl"
-								className="w-18 cursor-pointer bg-white"
-							>
-								<SelectValue placeholder="20" />
-							</SelectTrigger>
-							<SelectContent dir="rtl">
-								<SelectItem
-									value="10"
-									className="hover:cursor-pointer"
-								>
-									10
-								</SelectItem>
-								<SelectItem
-									value="20"
-									className="hover:cursor-pointer"
-								>
-									20
-								</SelectItem>
-								<SelectItem
-									value="50"
-									className="hover:cursor-pointer"
-								>
-									50
-								</SelectItem>
-								<SelectItem
-									value="100"
-									className="hover:cursor-pointer"
-								>
-									100
-								</SelectItem>
-							</SelectContent>
-						</Select>
-					</div>
-				</div>
-			</Formik>
-		</div>
+		// 				<Select
+		// 					defaultValue={"20"}
+		// 					value={String(resultPerPage)}
+		// 					onValueChange={(value) => {
+		// 						setResultPerPage(value);
+		// 					}}
+		// 				>
+		// 					<SelectTrigger
+		// 						dir="rtl"
+		// 						className="w-18 cursor-pointer bg-white"
+		// 					>
+		// 						<SelectValue placeholder="20" />
+		// 					</SelectTrigger>
+		// 					<SelectContent dir="rtl">
+		// 						<SelectItem
+		// 							value="10"
+		// 							className="hover:cursor-pointer"
+		// 						>
+		// 							10
+		// 						</SelectItem>
+		// 						<SelectItem
+		// 							value="20"
+		// 							className="hover:cursor-pointer"
+		// 						>
+		// 							20
+		// 						</SelectItem>
+		// 						<SelectItem
+		// 							value="50"
+		// 							className="hover:cursor-pointer"
+		// 						>
+		// 							50
+		// 						</SelectItem>
+		// 						<SelectItem
+		// 							value="100"
+		// 							className="hover:cursor-pointer"
+		// 						>
+		// 							100
+		// 						</SelectItem>
+		// 					</SelectContent>
+		// 				</Select>
+		// 			</div>
+		// 		</div>
+		// 	</Formik>
+		// </div>
 	);
 }
