90a8362192732559be5ba92945cc249a29c1d090
AminFiroozi <afiroozi007@gmail.com>
2025-08-01 13:11:27 +0330

    feat: add status filtering for new order

--- components/OrderHistory/OrderHistoryPagination.tsx
+++ components/OrderHistory/OrderHistoryPagination.tsx
@@ -13,39 +13,87 @@ import orderService from "@/src/services/orderService";
 import { useEffect, useState } from "react";
 import { Orderhistory } from "@/src/types/OrderhistoryType";
 import OrderHistory from "@/components/OrderHistory/OrderHistory";
-import { useSelector } from "react-redux";
 import panelNotFound from "../../public/images/panelNotFound/panelNotFound.png";
 import LoadingSpinner from "../Loading/LoadingSpinner/LoadingSpinner";
+import {
+	Select,
+	SelectContent,
+	SelectItem,
+	SelectTrigger,
+	SelectValue,
+} from "../ui/select";
+import { getData } from "@/src/services/apiHub";
 
-// import { RootState } from "@/src/store/types";
+interface status {
+	id: number;
+	name: string;
+}
 
 export default function OrderHistoryPagination() {
 	const [history, sethistory] = useState<Orderhistory[]>([]);
 	const [currpage, Setcurrpage] = useState<string>("1");
 	const [isLoading, setIsLoading] = useState(true);
+	const [statuses, setStatuses] = useState(null);
+	const [status, setStatus] = useState<string>("1");
 	const handelHistory = (status: string, offset: string, limit: string) => {
 		orderService
 			.orderHistory({
 				status: status ?? "1",
-				offset: status ?? "1",
-				limit: limit ?? "1",
+				offset: offset ?? "1",
+				limit: limit ?? "10",
 			})
 			.then((res) => {
 				sethistory(res?.data);
-				setIsLoading(false);
+				// setIsLoading(false);
 			})
-			.catch((err) => console.log(err));
+			.catch((err) => console.log(err))
+			.finally(() => setIsLoading(false));
 	};
 	useEffect(() => {
-		handelHistory("1", currpage, "10");
-	}, [currpage]);
+		setIsLoading(true);
+		getData({ endPoint: `/v1/installation/request/status` })
+			.then((data) => {
+				setStatuses(data?.data);
+				handelHistory(status, currpage, "10");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}, [status, currpage]);
 	return (
 		<>
+			{statuses && (
+				<Select
+					value={String(status)}
+					onValueChange={(value) => setStatus(value)}
+					data-test="warranty-filter"
+				>
+					<SelectTrigger
+						dir="rtl"
+						className="flex w-40 cursor-pointer place-self-end relative bg-gradient-to-br from-[#EBECF0] to-[#EFF0F2]"
+						data-test="warranty-filter-trigger"
+					>
+						<SelectValue placeholder="وضعیت گارانتی" />
+					</SelectTrigger>
+					<SelectContent dir="rtl">
+						{statuses?.map((status: status, index: number) => (
+							<SelectItem
+								key={index}
+								value={String(status.id)}
+								className="cursor-pointer"
+								data-test="warranty-filter-option-archived"
+							>
+								{status.name}
+							</SelectItem>
+						))}
+					</SelectContent>
+				</Select>
+			)}
 			{isLoading ? (
 				<LoadingSpinner />
 			) : history?.length > 0 ? (
 				<>
-					<div className="flex flex-col text-white bg-transparent">
+					<div className="flex flex-col bg-transparent">
 						<div className="flex flex-col text-gray-800 rounded-2xl overflow-auto shadow-[-6px_-6px_16px_rgba(255,255,255,0.8),6px_6px_16px_rgba(0,0,0,0.2)]">
 							{history.map((order: Orderhistory, index) => (
 								<OrderHistory
@@ -61,21 +109,36 @@ export default function OrderHistoryPagination() {
 					</div>
 				</>
 			) : (
-				<div className="text-center place-items-center mt-6">
+				<div className="relative text-center place-items-center py-18 bg-gradient-to-br from-[#EBECF0] to-[#EFF0F2]">
 					<Image
 						className="w-1/3"
 						src={panelNotFound}
 						alt="orderNotFound"
 					/>
-					<div className="-mt-8">
+					<div className="">
 						<p
-							className=" mt-6 text-navy-blue font-bold rtl"
+							className="mt-6 text-navy-blue font-bold rtl"
 							style={{ fontSize: "1.1rem" }}
 						>
-							هیچ سفارشی یافت نشد.
+							هیچ پنلی یافت نشد.
 						</p>
 					</div>
 				</div>
+				// <div className="text-center place-items-center mt-6">
+				// 	<Image
+				// 		className="w-1/3"
+				// 		src={panelNotFound}
+				// 		alt="orderNotFound"
+				// 	/>
+				// 	<div className="-mt-8">
+				// 		<p
+				// 			className=" mt-6 text-navy-blue font-bold rtl"
+				// 			style={{ fontSize: "1.1rem" }}
+				// 		>
+				// 			هیچ سفارشی یافت نشد.
+				// 		</p>
+				// 	</div>
+				// </div>
 			)}
 			{history?.length > 0 && (
 				<div className="p-5 rtl">
