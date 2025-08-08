b391dd462f1e06a0b5c232d970f3726c09bb483f
AminFiroozi <afiroozi007@gmail.com>
2025-08-08 11:42:41 +0330

    feat: add logout button

--- components/Navbar/DesktopNavbar/DesktopNavbar.tsx
+++ components/Navbar/DesktopNavbar/DesktopNavbar.tsx
@@ -5,10 +5,12 @@ import { vazirBold } from "@/lib/fonts";
 import { usePathname } from "next/navigation";
 import Dashboard from "./Dashboard/Dashboard";
 import { Skeleton } from "@/components/ui/skeleton";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import hasAdminAnyPermission from "@/src/functions/isAdmin";
+import { resetUser } from "@/src/store/slices/userSlice";
 
 export default function DesktopNavbar() {
+	const dispatch = useDispatch();
 	const [loading, setLoading] = useState<boolean>(true);
 	const [isCorp, setIsCorp] = useState<boolean>(false);
 	const pathname = usePathname();
@@ -37,52 +39,76 @@ export default function DesktopNavbar() {
 			<div className="h-[70px] fixed top-0 w-full flex flex-col justify-center items-center z-20">
 				<div className="flex justify-between items-center h-[70%] w-[94%] rounded-full mx-auto bg-white py-3 px-5 border-2 border-gray-300">
 					<div>{/* <Sun size={28} /> */}</div>
-					<div className="flex flex-row-reverse justify-start items-center w-[50%] gap-8">
+					<>
+						{/* <div className="flex flex-row-reverse justify-start items-center w-[50%] gap-8"> */}
 						{/* <CircleUserRound size={28} /> */}
 						{loading ? (
-							<>
-								<Skeleton className="h-[20px] w-[40px] rounded-full" />
-								<Skeleton className="h-[20px] w-[80px] rounded-full" />
-								<Skeleton className="h-[20px] w-[120px] rounded-full" />
-							</>
+							<div className="flex w-full justify-between">
+								{/* left side */}
+								<div className="flex flex-row-reverse justify-end items-center w-[50%] gap-8">
+									<Skeleton className="h-[20px] w-[60px] rounded-full" />
+								</div>
+								{/* right side */}
+								<div className="flex flex-row-reverse justify-start items-center w-[50%] gap-8">
+									<Skeleton className="h-[20px] w-[40px] rounded-full" />
+									<Skeleton className="h-[20px] w-[80px] rounded-full" />
+									<Skeleton className="h-[20px] w-[120px] rounded-full" />
+								</div>
+							</div>
 						) : (
-							<>
-								<Link
-									className={[
-										pathname === "/"
-											? "text-[#FA682D]"
-											: "",
-										vazirBold.className,
-									].join(" ")}
-									href={"/"}
-								>
-									خانه
-								</Link>
-								{!accessToken && (
+							<div className="flex w-full justify-between">
+								<div className="flex flex-row-reverse justify-end items-center w-[50%] gap-8">
+									{accessToken && (
+										<Link
+											className={`
+										${vazirBold.className}`}
+											href={"/login"}
+											onClick={() =>
+												dispatch(resetUser())
+											}
+										>
+											خروج
+										</Link>
+									)}
+								</div>
+								<div className="flex flex-row-reverse justify-start items-center w-[50%] gap-8">
 									<Link
-										className={`vazir-bold`}
-										href={"/login"}
+										className={[
+											pathname === "/"
+												? "text-[#FA682D]"
+												: "",
+											vazirBold.className,
+										].join(" ")}
+										href={"/"}
 									>
-										ورود
+										خانه
 									</Link>
-								)}
-								<Dashboard
-									accessToken={accessToken}
-									isCorp={isCorp}
-									isAdmin={isAdmin}
-								/>
-								<Link
-									className={`vazir-bold rtl ${
-										pathname ===
-										"/landing/corp-introduction"
-											? "text-[#FA682D]"
-											: ""
-									}`}
-									href={"/landing/corp-introduction"}
-								>
-									در برق نو بفروشید!
-								</Link>
-							</>
+									{!accessToken && (
+										<Link
+											className={`vazir-bold`}
+											href={"/login"}
+										>
+											ورود
+										</Link>
+									)}
+									<Dashboard
+										accessToken={accessToken}
+										isCorp={isCorp}
+										isAdmin={isAdmin}
+									/>
+									<Link
+										className={`vazir-bold rtl ${
+											pathname ===
+											"/landing/corp-introduction"
+												? "text-[#FA682D]"
+												: ""
+										}`}
+										href={"/landing/corp-introduction"}
+									>
+										در برق نو بفروشید!
+									</Link>
+								</div>
+							</div>
 						)}
 						{/* <Link
 							className={[
@@ -117,7 +143,7 @@ export default function DesktopNavbar() {
 						>
 							داشبورد ادمین
 						</Link> */}
-					</div>
+					</>
 				</div>
 			</div>
 		</>
