5569c323c12642ce90ddcf7b0f00c799c71bcd5d
AminFiroozi <afiroozi007@gmail.com>
2025-08-09 00:34:53 +0330

    fix: blog and announcements

--- app/admin-dashboard/blogs/page.tsx
+++ app/admin-dashboard/blogs/page.tsx
@@ -3,28 +3,28 @@ export const dynamic = "force-dynamic";
 
 import nextDynamic from "next/dynamic";
 const BlogCard = nextDynamic(
-	() => import("@/components/blog/BlogCard/BlogCard"),
-	{
-		ssr: false,
-	}
+    () => import("@/components/blog/BlogCard/BlogCard"),
+    {
+        ssr: false,
+    }
 );
 
 const PageContainer = nextDynamic(
-	() => import("@/components/Dashboard/PageContainer/PageContainer"),
-	{
-		ssr: false,
-	}
+    () => import("@/components/Dashboard/PageContainer/PageContainer"),
+    {
+        ssr: false,
+    }
 );
 
 const Header = nextDynamic(() => import("@/components/Header/Header"), {
-	ssr: false,
+    ssr: false,
 });
 
 const LoadingSpinner = nextDynamic(
-	() => import("@/components/Loading/LoadingSpinner/LoadingSpinner"),
-	{
-		ssr: false,
-	}
+    () => import("@/components/Loading/LoadingSpinner/LoadingSpinner"),
+    {
+        ssr: false,
+    }
 );
 
 import { getData } from "@/src/services/apiHub";
@@ -33,50 +33,49 @@ import React from "react";
 import useClientCheck from "@/src/hooks/useClientCheck";
 
 export default function Page() {
-	const isClient = useClientCheck();
-	const { isLoading, data, error } = useQuery({
-		queryKey: ["blogs"],
-		queryFn: async () => {
-			const r1 = await getData({
-				endPoint: `/v1/blog`,
-			});
-			console.log("r1: ", r1);
-			return r1;
-		},
-	});
-	// return <div>meow</div>
-	return isClient ? (
-		<PageContainer>
-			{isLoading && <LoadingSpinner />}
-
-			{!isLoading && (
-				<>
-					<Header header="مطالب" />
-					<div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-[2vw] w-full mx-auto">
-						{data?.data?.map((blog: Blog) => {
-							return (
-								<BlogCard
-									key={blog.id}
-									blogID={String(blog.id)}
-									imageUrl={blog.cover_image}
-									title={blog.title}
-									description={blog.description}
-									writer={
-										blog.author ?? blog.corporation?.name
-									}
-									date={blog.created_at}
-									likeCount={blog.like_count}
-									status={blog.status}
-									viewOnly={true}
-									className="w-[70vw] md:w-[40vw] mx-auto"
-								/>
-							);
-						})}
-					</div>
-				</>
-			)}
-		</PageContainer>
-	) : (
-		<></>
-	);
+    const isClient = useClientCheck();
+    const { isLoading, data, error } = useQuery({
+        queryKey: ["blogs"],
+        queryFn: async () => {
+            const r1 = await getData({
+                endPoint: `/v1/blog`,
+            });
+            console.log("r1: ", r1?.data?.data);
+            return r1?.data?.data;
+        },
+    });
+    return (
+        <PageContainer>
+            {isClient && isLoading ? (
+                <LoadingSpinner />
+            ) : (
+                <>
+                    <Header header="مطالب" />
+                    <div className="grid grid-cols-1 lg:grid-cols-2 lg:gap-x-[2vw] w-full mx-auto">
+                        {data &&
+                            data?.map((blog: Blog) => {
+                                return (
+                                    <BlogCard
+                                        key={blog?.id}
+                                        blogID={String(blog?.id)}
+                                        imageUrl={blog?.coverImage}
+                                        title={blog?.title}
+                                        description={blog?.description}
+                                        writer={
+                                            blog?.author ??
+                                            blog?.corporation?.name
+                                        }
+                                        date={blog?.createdAt}
+                                        likeCount={blog?.likeCount}
+                                        status={blog?.status}
+                                        viewOnly={true}
+                                        className="mx-auto"
+                                    />
+                                );
+                            })}
+                    </div>
+                </>
+            )}
+        </PageContainer>
+    );
 }
