001c83774a9734dcf1a1dc9dc32af97bc127b501
AminFiroozi <afiroozi007@gmail.com>
2025-08-10 00:01:24 +0330

    fix: blog card width in customer dashboard

--- next.config.ts
+++ next.config.ts
@@ -1,56 +1,68 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	reactStrictMode: false,
-	eslint: {
-		// Warning: This allows production builds to successfully complete even if
-		// your project has ESLint errors.
-		ignoreDuringBuilds: true,
-	},
-	webpack: (config) => {
-		config.ignoreWarnings = [{ module: /@opentelemetry\/instrumentation/ }];
-		return config;
-	},
-	images: {
-		domains: [
-			"barghe-no-profile-pic.s3.ir-thr-at1.arvanstorage.ir",
-			"barghe-no-ticket-image.s3.ir-thr-at1.arvanstorage.ir",
-		],
-
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname:
-					"barghe-no-ticket-image.s3.ir-thr-at1.arvanstorage.ir",
-			},
-			{
-				protocol: "https",
-				hostname: "barghe-no-blog-media.s3.ir-thr-at1.arvanstorage.ir",
-			},
-			{
-				protocol: "https",
-				hostname:
-					"barghe-no-taxpayer-certificate.s3.ir-thr-at1.arvanstorage.ir",
-			},
-			{
-				protocol: "https",
-				hostname:
-					"barghe-no-official-newspaper-ad.s3.ir-thr-at1.arvanstorage.ir",
-			},
-			{
-				protocol: "https",
-				hostname: "barghe-no-logo-pic.s3.ir-thr-at1.arvanstorage.ir",
-			},
-			{
-				protocol: "https",
-				hostname: "barghe-no-news-media.s3.ir-thr-at1.arvanstorage.ir",
-			},
-			{
-				protocol: "https",
-				hostname: "barghe-no-profile-pic.s3.ir-thr-at1.arvanstorage.ir",
-			},
-		],
-	},
+    reactStrictMode: false,
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    webpack: (config) => {
+        config.ignoreWarnings = [{ module: /@opentelemetry\/instrumentation/ }];
+        return config;
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**", // Allow all HTTPS domains
+            },
+            {
+                protocol: "http",
+                hostname: "**", // Allow all HTTP domains (optional)
+            },
+        ],
+        // domains: [
+        //     "barghe-no-profile-pic.s3.ir-thr-at1.arvanstorage.ir",
+        //     "barghe-no-ticket-image.s3.ir-thr-at1.arvanstorage.ir",
+        //     "barghe-no-blog-media.s3.ir-thr-at1.arvanstorage.ir",
+        //     "barghe-no-taxpayer-certificate.s3.ir-thr-at1.arvanstorage.ir",
+        //     "barghe-no-news-media.s3.ir-thr-at1.arvanstorage.ir",
+        //     "barghe-no-logo-pic.s3.ir-thr-at1.arvanstorage.ir",
+        //     "barghe-no-official-newspaper-ad.s3.ir-thr-at1.arvanstorage.ir",
+        // ],
+        // remotePatterns: [
+        //     {
+        //         protocol: "https",
+        //         hostname:
+        //             "barghe-no-ticket-image.s3.ir-thr-at1.arvanstorage.ir",
+        //     },
+        //     {
+        //         protocol: "https",
+        //         hostname: "barghe-no-blog-media.s3.ir-thr-at1.arvanstorage.ir",
+        //     },
+        //     {
+        //         protocol: "https",
+        //         hostname:
+        //             "barghe-no-taxpayer-certificate.s3.ir-thr-at1.arvanstorage.ir",
+        //     },
+        //     {
+        //         protocol: "https",
+        //         hostname:
+        //             "barghe-no-official-newspaper-ad.s3.ir-thr-at1.arvanstorage.ir",
+        //     },
+        //     {
+        //         protocol: "https",
+        //         hostname: "barghe-no-logo-pic.s3.ir-thr-at1.arvanstorage.ir",
+        //     },
+        //     {
+        //         protocol: "https",
+        //         hostname: "barghe-no-news-media.s3.ir-thr-at1.arvanstorage.ir",
+        //     },
+        //     {
+        //         protocol: "https",
+        //         hostname: "barghe-no-profile-pic.s3.ir-thr-at1.arvanstorage.ir",
+        //     },
+        // ],
+    },
 };
 
 export default nextConfig;
