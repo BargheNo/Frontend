94a7e85f772d6d267603749770444a83263e42b6
AminFiroozi <afiroozi007@gmail.com>
2025-08-10 00:55:57 +0330

    fix: add change corp component

--- app/corpdashboard/layout.tsx
+++ app/corpdashboard/layout.tsx
@@ -5,21 +5,21 @@ import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { CorpNavItems } from "@/src/constants/navItems";
 import { getData } from "@/src/services/apiHub";
-import { setCorpId } from "@/src/store/slices/userSlice";
+// import { setCorpId } from "@/src/store/slices/userSlice";
 
 export default function Layout({
     children,
 }: Readonly<{ children: React.ReactNode }>) {
-    const dispatch = useDispatch();
-    useEffect(() => {
-        getData({ endPoint: `/v1/user/corps` })
-            .then((res) => {
-                console.log("rescorp", res?.data[0]?.id);
-                // const corpId = res?.data[0]?.id;
-                dispatch(setCorpId(res?.data[0]?.id));
-            })
-            .catch((err) => console.log(err));
-    }, [dispatch]);
+    // const dispatch = useDispatch();
+    // useEffect(() => {
+    //     getData({ endPoint: `/v1/user/corps` })
+    //         .then((res) => {
+    //             console.log("rescorp", res?.data[0]?.id);
+    //             // const corpId = res?.data[0]?.id;
+    //             dispatch(setCorpId(res?.data[0]?.id));
+    //         })
+    //         .catch((err) => console.log(err));
+    // }, [dispatch]);
 
     return (
         <PanelAside navItems={CorpNavItems} mode="corp">
--- components/Panel/PanelAside/PanelAside.tsx
+++ components/Panel/PanelAside/PanelAside.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
@@ -12,167 +12,236 @@ import { InteractiveGridPattern } from "@/components/Background/InteractiveGridP
 import { PanelAsideProps, NavItem } from "@/src/types/PanelAsideTypes";
 import styles from "./PanelAside.module.css";
 import {
-	SidebarContent,
-	Sidebar,
-	SidebarProvider,
+    SidebarContent,
+    Sidebar,
+    SidebarProvider,
 } from "@/components/ui/sidebar";
 import { Activity, Columns2 } from "lucide-react";
 import {
-	Accordion,
-	AccordionContent,
-	AccordionItem,
-	AccordionTrigger,
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
 } from "@/components/ui/accordion";
 import {
-	PanelAsideIcon,
-	PanelAsideTitle,
+    PanelAsideIcon,
+    PanelAsideTitle,
 } from "./PanelAsideItem/PanelAsideItem";
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select";
+import { useDispatch } from "react-redux";
+import { getData } from "@/src/services/apiHub";
+import { setCorpId } from "@/src/store/slices/userSlice";
 const myFont = localFont({ src: "../../../public/fonts/vazir/Vazir.ttf" });
 
+interface Corp {
+    id: number;
+    name: string;
+}
+
 const PanelAside = ({
-	children,
-	navItems,
-	navItemsMonitoring,
-	mode = "customer",
+    children,
+    navItems,
+    navItemsMonitoring,
+    mode = "customer",
 }: PanelAsideProps) => {
-	const pathname = usePathname();
-	const [isMobile, setIsMobile] = useState(false);
-	const [sideOpen, setSideOpen] = useState(false);
+    const dispatch = useDispatch();
+    const pathname = usePathname();
+    const [isMobile, setIsMobile] = useState(false);
+    const [sideOpen, setSideOpen] = useState(false);
+    const [corp, setCorp] = useState<string>("0");
+    const [corps, setCorps] = useState<Corp[]>([]);
 
-	useEffect(() => {
-		const checkMobile = () => {
-			setIsMobile(window.innerWidth < 768); // 768px is typical mobile breakpoint
-		};
+    useEffect(() => {
+        const checkMobile = () => {
+            setIsMobile(window.innerWidth < 768); // 768px is typical mobile breakpoint
+        };
 
-		// Check on mount
-		checkMobile();
+        // Check on mount
+        checkMobile();
 
-		// Add resize listener
-		window.addEventListener("resize", checkMobile);
+        // Add resize listener
+        window.addEventListener("resize", checkMobile);
 
-		// Cleanup
-		return () => window.removeEventListener("resize", checkMobile);
-	}, []);
+        // Cleanup
+        return () => window.removeEventListener("resize", checkMobile);
+    }, []);
+    const changeCorp = useCallback(
+        (corpId: string) => {
+            setCorp(String(corpId));
+            dispatch(setCorpId(Number(corpId)));
+        },
+        [dispatch]
+    );
 
-  if (isMobile) {
-    return <main className="rtl w-screen pb-18">{children}</main>;
-  }
+    useEffect(() => {
+        getData({ endPoint: `/v1/user/corps` })
+            .then((res) => {
+                console.log(res?.data);
+                setCorps(res?.data);
+                changeCorp(res?.data?.[0]?.id);
+                // setCorp(String(res?.data?.[0]?.id));
+                // console.log("rescorp", res?.data[0]?.id);
+                // const corpId = res?.data[0]?.id;
+                // dispatch(setCorpId(res?.data[0]?.id));
+            })
+            .catch((err) => console.log(err));
+    }, [dispatch, changeCorp]);
+    if (isMobile) {
+        return <main className="rtl w-screen pb-18">{children}</main>;
+    }
 
-	return (
-		<div
-			className={`flex fixed h-full w-full bg-[#F0EDEF] under-navbar-content ${myFont.className}`}
-			dir="rtl"
-		>
-			<SidebarProvider open={sideOpen}>
-				<Sidebar
-					side="right"
-					className="w-64 bg-[#F0EDEF] text-white p-2 z-10 under-navbar-content border-0!"
-				>
-					<SidebarContent className="bg-[#F0EDEF] no-scrollbar">
-						<nav className="space-y-2 bg-transparent">
-							{navItems.map((item: NavItem, index) => (
-								<PanelAsideTitle
-									item={item}
-									mode={mode}
-									key={index}
-								/>
-							))}
-							{mode == "admin" && (
-								<Accordion
-									type="single"
-									collapsible
-									className="w-full no-underline"
-								>
-									<AccordionItem value="item-1">
-										<AccordionTrigger
-											className={`flex gap-2 text-md text-[#003a8b] p-2 mt-0.5 rounded-lg no-underline cursor-pointer ${
-												pathname ===
-												"/admin-dashboard/monitoring"
-													? `shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] bg-gradient-to-r ${"from-[#FF5B18] to-[#FF6809]"} text-white`
-													: "hover:shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.5),inset_1px_1px_3px_rgba(0,0,0,0.2)] duration-200"
-											}`}
-										>
-											پایش اطلاعات
-										</AccordionTrigger>
-										<AccordionContent className="flex flex-col text-base mr-4">
-											{navItemsMonitoring?.map(
-												(item: NavItem, index) => (
-													<Link
-														key={item.path}
-														href={item.path}
-													>
-														<span
-															className={`flex gap-2 text-[#003a8b] p-2 mt-0.5 rounded-lg cursor-pointer ${
-																pathname ===
-																item.path
-																	? `shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] bg-gradient-to-r ${"from-[#FF5B18] to-[#FF6809]"} text-white`
-																	: "hover:shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.5),inset_1px_1px_3px_rgba(0,0,0,0.2)] duration-200"
-															}`}
-														>
-															{item.name}
-														</span>
-													</Link>
-												)
-											)}
-										</AccordionContent>
-									</AccordionItem>
-								</Accordion>
-							)}
-						</nav>
-					</SidebarContent>
-				</Sidebar>
-				<div
-					className={`${
-						sideOpen ? "ml-3" : "mx-3"
-					} flex flex-col items-center justify-between h-[100vh]`}
-				>
-					<div>
-						{navItems.map((item: NavItem, index) => (
-							<PanelAsideIcon
-								item={item}
-								mode={mode}
-								key={index}
-							/>
-						))}
-						{mode == "admin" && (
-							<PanelAsideIcon
-								item={{ name: "", path: "", icon: <Activity /> }}
-								mode={mode}
-							/>
-						)}
-					</div>
-					<span className="cursor-pointer mb-22 shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] rounded-lg p-1">
-						<Columns2
-							size={30}
-							color={`${sideOpen ? "#F37240" : "#003a8b"} `}
-							className="transition-colors duration-300"
-							onClick={() => {
-								setSideOpen((state) => !state);
-							}}
-						/>
-					</span>
-				</div>
+    return (
+        <div
+            className={`flex fixed h-full w-full bg-[#F0EDEF] under-navbar-content ${myFont.className}`}
+            dir="rtl"
+        >
+            <SidebarProvider open={sideOpen}>
+                <Sidebar
+                    side="right"
+                    className="w-64 bg-[#F0EDEF] text-white p-2 z-10 under-navbar-content border-0!"
+                >
+                    <SidebarContent className="bg-[#F0EDEF] no-scrollbar flex justify-between">
+                        <nav className="space-y-2 bg-transparent">
+                            {navItems.map((item: NavItem, index) => (
+                                <PanelAsideTitle
+                                    item={item}
+                                    mode={mode}
+                                    key={index}
+                                />
+                            ))}
+                            {mode == "admin" && (
+                                <Accordion
+                                    type="single"
+                                    collapsible
+                                    className="w-full no-underline"
+                                >
+                                    <AccordionItem value="item-1">
+                                        <AccordionTrigger
+                                            className={`flex gap-2 text-md text-[#003a8b] p-2 mt-0.5 rounded-lg no-underline cursor-pointer ${
+                                                pathname ===
+                                                "/admin-dashboard/monitoring"
+                                                    ? `shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] bg-gradient-to-r ${"from-[#FF5B18] to-[#FF6809]"} text-white`
+                                                    : "hover:shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.5),inset_1px_1px_3px_rgba(0,0,0,0.2)] duration-200"
+                                            }`}
+                                        >
+                                            پایش اطلاعات
+                                        </AccordionTrigger>
+                                        <AccordionContent className="flex flex-col text-base mr-4">
+                                            {navItemsMonitoring?.map(
+                                                (item: NavItem, index) => (
+                                                    <Link
+                                                        key={item.path}
+                                                        href={item.path}
+                                                    >
+                                                        <span
+                                                            className={`flex gap-2 text-[#003a8b] p-2 mt-0.5 rounded-lg cursor-pointer ${
+                                                                pathname ===
+                                                                item.path
+                                                                    ? `shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] bg-gradient-to-r ${"from-[#FF5B18] to-[#FF6809]"} text-white`
+                                                                    : "hover:shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.5),inset_1px_1px_3px_rgba(0,0,0,0.2)] duration-200"
+                                                            }`}
+                                                        >
+                                                            {item.name}
+                                                        </span>
+                                                    </Link>
+                                                )
+                                            )}
+                                        </AccordionContent>
+                                    </AccordionItem>
+                                </Accordion>
+                            )}
+                        </nav>
+                        {mode === "corp" && (
+                            <Select
+                                value={corp}
+                                onValueChange={(value) => {
+                                    changeCorp(value);
+                                }}
+                            >
+                                <SelectTrigger
+                                    dir="rtl"
+                                    className="text-black w-full shadow-inner shadow-[rgba(0,0,0,0.2)] mb-3 text-right bg-white"
+                                >
+                                    <SelectValue placeholder="انتخاب شرکت" />
+                                </SelectTrigger>
+                                <SelectContent dir="rtl">
+                                    {corps &&
+                                        corps?.map((c, index: number) => (
+                                            <SelectItem
+                                                key={index}
+                                                value={String(c?.id)}
+                                                className="cursor-pointer"
+                                            >
+                                                {c?.name}
+                                            </SelectItem>
+                                        ))}
+                                </SelectContent>
+                            </Select>
+                        )}
+                    </SidebarContent>
+                </Sidebar>
+                <div
+                    className={`${
+                        sideOpen ? "ml-3" : "mx-3"
+                    } flex flex-col items-center justify-between h-[100vh]`}
+                >
+                    <div>
+                        {navItems.map((item: NavItem, index) => (
+                            <PanelAsideIcon
+                                item={item}
+                                mode={mode}
+                                key={index}
+                            />
+                        ))}
+                        {mode == "admin" && (
+                            <PanelAsideIcon
+                                item={{
+                                    name: "",
+                                    path: "",
+                                    icon: <Activity />,
+                                }}
+                                mode={mode}
+                            />
+                        )}
+                    </div>
+                    <span className="cursor-pointer mb-22 shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] rounded-lg p-1">
+                        <Columns2
+                            size={30}
+                            color={`${sideOpen ? "#F37240" : "#003a8b"} `}
+                            className="transition-colors duration-300"
+                            onClick={() => {
+                                setSideOpen((state) => !state);
+                            }}
+                        />
+                    </span>
+                </div>
 
-				<main className="rounded-xl ml-3 mb-2 flex-1 bg-white relative">
-					<div className="absolute no-scrollbar overflow-y-auto top-0 left-0 right-0 bottom-0 rounded-xl bg-white z-10">
-						<InteractiveGridPattern
-							width={40}
-							height={40}
-							squares={[50, 50]}
-							x={-1}
-							y={-1}
-							strokeDasharray={"4 2"}
-							className={cn(
-								"absolute inset-0 h-full w-full [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]"
-							)}
-						/>
-						{children}
-					</div>
-					<div className="absolute inset-0 rounded-xl pointer-events-none shadow-[inset_-4px_-4px_5px_rgba(255,255,255,1),inset_4px_4px_5px_rgba(0,0,0,0.3)] z-20"></div>
-				</main>
-			</SidebarProvider>
-		</div>
-	);
+                <main className="rounded-xl ml-3 mb-2 flex-1 bg-white relative">
+                    <div className="absolute no-scrollbar overflow-y-auto top-0 left-0 right-0 bottom-0 rounded-xl bg-white z-10">
+                        <InteractiveGridPattern
+                            width={40}
+                            height={40}
+                            squares={[50, 50]}
+                            x={-1}
+                            y={-1}
+                            strokeDasharray={"4 2"}
+                            className={cn(
+                                "absolute inset-0 h-full w-full [mask-image:radial-gradient(900px_circle_at_center,white,transparent)]"
+                            )}
+                        />
+                        {children}
+                    </div>
+                    <div className="absolute inset-0 rounded-xl pointer-events-none shadow-[inset_-4px_-4px_5px_rgba(255,255,255,1),inset_4px_4px_5px_rgba(0,0,0,0.3)] z-20"></div>
+                </main>
+            </SidebarProvider>
+        </div>
+    );
 };
 
 export default PanelAside;
