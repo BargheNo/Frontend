25f3659b55d267ede2b0886a1b9601e9541f5f8a
AminFiroozi <afiroozi007@gmail.com>
2025-08-10 23:52:39 +0330

    fix: API error

--- components/admin-dashboard/RolesAndPermissions/EditRoleModal.tsx
+++ components/admin-dashboard/RolesAndPermissions/EditRoleModal.tsx
@@ -79,7 +79,9 @@ const EditRoleModal: React.FC<EditRoleModalProps> = ({
 	const getAllPermissions = async () => {
 		getData({ endPoint: `/v1/admin/permissions` })
 			.then((data) => {
-				setAllPermissions(data.data);
+
+				// console.log(data?.data);
+				setAllPermissions(data?.data?.data);
 			})
 			.catch((err) => console.log(err));
 	};
@@ -127,7 +129,7 @@ const EditRoleModal: React.FC<EditRoleModalProps> = ({
 	};
 
 	// Group permissions by category
-	const permissionsByCategory = allPermissions.reduce((acc, permission) => {
+	const permissionsByCategory = allPermissions?.reduce((acc, permission) => {
 		if (!acc[permission.category]) {
 			acc[permission.category] = [];
 		}
