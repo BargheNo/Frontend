2b704a2e6dfa9fb2f235ea2c602eb1c05b619829
AminFiroozi <afiroozi007@gmail.com>
2025-08-11 14:54:31 +0330

    ops: add build error got R&P

--- components/admin-dashboard/RolesAndPermissions/RolesAndPermissions.tsx
+++ components/admin-dashboard/RolesAndPermissions/RolesAndPermissions.tsx
@@ -29,7 +29,7 @@ import {
     SelectItem,
     SelectTrigger,
     SelectValue,
-} from "@/components/ui/select";
+} from "@/components/ui/select/r";
 import {
     Command,
     CommandEmpty,
@@ -157,7 +157,7 @@ const RolesAndPermissions = () => {
                             aria-expanded={open}
                             className="min-w-40 relative rtl bg-gradient-to-br from-[#EBECF0] to-[#EFF0F2] justify-between gap-2"
                         >
-                            {permissionFilter === "all" ? "همه" : ""}}
+                            {permissionFilter === "all" ? "همه" : ""}
                             {permissionFilter
                                 ? allPermissions.find(
                                       (perm) =>
