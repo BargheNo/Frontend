0137d1d1a097c6fdcbb4152f38a858b9ca8f7bf9
erfan.teikst <erfan.teikst2027@gmail.com>
2025-08-11 19:24:52 +0330

    Refactor: enhance layout structure and improve AnnounceEditor component

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,34 +10,34 @@ import { usePathname } from "next/navigation";
 import { WebSocketProvider } from "@/components/WebSocketProvider/WebSocketProvider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-	const [queryClient] = useState(
-		() =>
-			new QueryClient({
-				defaultOptions: {
-					// queries: {
-					//   staleTime: 60 * 1000, // 1 minute
-					//   retry: 1,
-					// },
-				},
-			})
-	);
-	const pathname = usePathname();
-	if (pathname?.startsWith("/announcements")) {
-		return (
-			<QueryClientProvider client={queryClient}>
-				<Provider store={store}>
-					<WebSocketProvider>{children}</WebSocketProvider>
-				</Provider>
-			</QueryClientProvider>
-		);
-	}
-	return (
-		<QueryClientProvider client={queryClient}>
-			<Provider store={store}>
-				<WebSocketProvider>
-					<SidebarProvider>{children}</SidebarProvider>
-				</WebSocketProvider>
-			</Provider>
-		</QueryClientProvider>
-	);
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    // queries: {
+                    //   staleTime: 60 * 1000, // 1 minute
+                    //   retry: 1,
+                    // },
+                },
+            })
+    );
+    // const pathname = usePathname();
+    // if (pathname?.startsWith("/announcements")) {
+    // 	return (
+    // 		<QueryClientProvider client={queryClient}>
+    // 			<Provider store={store}>
+    // 				<WebSocketProvider>{children}</WebSocketProvider>
+    // 			</Provider>
+    // 		</QueryClientProvider>
+    // 	);
+    // }
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Provider store={store}>
+                <WebSocketProvider>
+                    <SidebarProvider>{children}</SidebarProvider>
+                </WebSocketProvider>
+            </Provider>
+        </QueryClientProvider>
+    );
 }
