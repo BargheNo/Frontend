db141870150bcd7989cbba72ba96031bea695442
Tina.2017 <tina.mpmp2017@gmail.com>
2025-08-12 03:46:54 +0330

    fix:made the bids page rosponsive for all devices

--- components/CorpDashboard/Bids/BidCard.tsx
+++ components/CorpDashboard/Bids/BidCard.tsx
@@ -159,14 +159,14 @@ const Item = ({
             </div>
             <div className="flex gap-1 items-start">
                 <span
-                    className={`${smallValue ? "text-xl" : "text-xl mt-[2px]"}`}
+                    className={`text-nowrap ${smallValue ? "text-xl" : "text-xl mt-[2px]"}`}
                 >
                     {fieldName}:{" "}
                 </span>
                 <span
                     dir={english ? "ltr" : "rtl"}
                     className={`text-black ${
-                        smallValue ? "mt-[3px]" : "font-bold text-2xl"
+                        smallValue ? "mt-[3px]" : "font-bold md:text-xl text-l"
                     }`}
                 >
                     {value}
@@ -308,7 +308,7 @@ export default function BidCard({
             className={`w-full min-h-64 border-t-1 border-gray-300 first:border-t-0`}
         >
             <div className="flex flex-row justify-between w-full min-h-64 bg-[#F0EDEF] overflow-hidden relative">
-                <div className="flex flex-row justify-between w-full min-h-64">
+                <div className="flex md:flex-row flex-col justify-between w-full min-h-64">
                     <div className="w-4/5 flex flex-col justify-between px-4 py-4 h-full">
                         <Item
                             icon={Eclipse}
@@ -341,8 +341,8 @@ export default function BidCard({
                             prefix="تومان"
                         />
                     </div>
-                    <div className="flex flex-col justify-evenly w-1/5 items-center text-center">
-                        <div className="flex flex-col items-center justify-center gap-2 p-3 rounded-2xl bg-[#F0F0F3] shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.8),inset_4px_4px_10px_rgba(0,0,0,0.1)] w-32">
+                    <div className="flex md:flex-col flex-row justify-evenly w-1/5 items-center text-center md:mr-0 mr-6 md:gap-0 gap-40 md:mb-0 mb-10">
+                        <div className="flex flex-col  md:ml-9 ml-auto items-center justify-center gap-2 md:p-3 p-5 rounded-2xl bg-[#F0F0F3] shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.8),inset_4px_4px_10px_rgba(0,0,0,0.1)] w-37">
                             <div
                                 className={`h-4 w-4 rounded-full ${getStatusColor()} shadow-md`}
                             />
@@ -352,7 +352,7 @@ export default function BidCard({
                         </div>
                         <Dialog open={open} onOpenChange={setOpen}>
                             <DialogTrigger asChild>
-                                <div className="flex flex-col items-center gap-2">
+                                <div className="flex flex-col items-center gap-2 md:mr-0 -mr-5 text-nowrap">
                                     <div className="bg-gradient-to-b from-[#EE4334] to-[#D73628] rounded-full w-16 h-16 flex items-center place-content-center text-white cursor-pointer shadow-md hover:shadow-lg transition duration-300 hover:scale-105">
                                         <ArrowLeft />
                                     </div>
