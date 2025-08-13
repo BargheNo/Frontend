1907224e37856a245aaf1b579097ec0df387766a
AminFiroozi <afiroozi007@gmail.com>
2025-08-13 16:59:27 +0330

    fix: add tooltip for when a corp is suspended

--- components/Panel/PanelAside/PanelAsideItem/PanelAsideItem.tsx
+++ components/Panel/PanelAside/PanelAsideItem/PanelAsideItem.tsx
@@ -3,6 +3,28 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
 import { NavItem } from "@/src/types/PanelAsideTypes";
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+const ToolTipWrapper = ({
+    notAllowed,
+    children,
+}: {
+    notAllowed: boolean;
+    children: React.ReactNode;
+}) => {
+    return notAllowed ? (
+        <Tooltip>
+            <TooltipTrigger asChild>{children}</TooltipTrigger>
+            <TooltipContent>شرکت شما در حالت تعلیق است.</TooltipContent>
+        </Tooltip>
+    ) : (
+        <>{children}</>
+    );
+};
 
 export function PanelAsideIcon({
     item,
@@ -21,25 +43,27 @@ export function PanelAsideIcon({
     if (!hasPermission) return <></>;
 
     return (
-        <Link key={item.path} href={notAllowed ? item.path : ""}>
-            <span
-                className={`flex gap-2 text-[#003a8b] p-2 mt-0.5 rounded-lg cursor-${
-                    notAllowed ? "not-allowed" : "pointer"
-                } ${
-                    pathname === item.path
-                        ? `shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] bg-gradient-to-r ${
-                              mode === "customer"
-                                  ? "from-[#A55FDA] to-[#F37240]"
-                                  : mode === "corp"
-                                  ? "from-[#2979FF] to-[#1b6cf5]"
-                                  : "from-[#FF5B18] to-[#FF6809]"
-                          } text-white`
-                        : "hover:shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.5),inset_1px_1px_3px_rgba(0,0,0,0.2)] duration-200"
-                }`}
-            >
-                {item?.icon}
-            </span>
-        </Link>
+        <ToolTipWrapper notAllowed={notAllowed}>
+            <Link key={item.path} href={notAllowed ? item.path : ""}>
+                <span
+                    className={`flex gap-2 text-[#003a8b] p-2 mt-0.5 rounded-lg cursor-${
+                        notAllowed ? "not-allowed" : "pointer"
+                    } ${
+                        pathname === item.path
+                            ? `shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] bg-gradient-to-r ${
+                                  mode === "customer"
+                                      ? "from-[#A55FDA] to-[#F37240]"
+                                      : mode === "corp"
+                                      ? "from-[#2979FF] to-[#1b6cf5]"
+                                      : "from-[#FF5B18] to-[#FF6809]"
+                              } text-white`
+                            : "hover:shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.5),inset_1px_1px_3px_rgba(0,0,0,0.2)] duration-200"
+                    }`}
+                >
+                    {item?.icon}
+                </span>
+            </Link>
+        </ToolTipWrapper>
     );
 }
 
@@ -60,24 +84,26 @@ export function PanelAsideTitle({
     if (!hasPermission) return <></>;
 
     return (
-        <Link key={item.path} href={notAllowed ? item.path : ""}>
-            <span
-                className={`flex gap-2 text-[#003a8b] p-2 mt-0.5 rounded-lg cursor-${
-                    notAllowed ? "not-allowed" : "pointer"
-                } ${
-                    pathname === item.path
-                        ? `shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] bg-gradient-to-r ${
-                              mode === "customer"
-                                  ? "from-[#A55FDA] to-[#F37240]"
-                                  : mode === "corp"
-                                  ? "from-[#2979FF] to-[#1b6cf5]"
-                                  : "from-[#FF5B18] to-[#FF6809]"
-                          } text-white`
-                        : "hover:shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.5),inset_1px_1px_3px_rgba(0,0,0,0.2)] duration-200"
-                }`}
-            >
-                {item.name}
-            </span>
-        </Link>
+        <ToolTipWrapper notAllowed={notAllowed}>
+            <Link key={item.path} href={notAllowed ? item.path : ""}>
+                <span
+                    className={`flex gap-2 text-[#003a8b] p-2 mt-0.5 rounded-lg cursor-${
+                        notAllowed ? "not-allowed" : "pointer"
+                    } ${
+                        pathname === item.path
+                            ? `shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.3),inset_1px_1px_3px_rgba(0,0,0,0.3)] bg-gradient-to-r ${
+                                  mode === "customer"
+                                      ? "from-[#A55FDA] to-[#F37240]"
+                                      : mode === "corp"
+                                      ? "from-[#2979FF] to-[#1b6cf5]"
+                                      : "from-[#FF5B18] to-[#FF6809]"
+                              } text-white`
+                            : "hover:shadow-[inset_-4px_-4px_10px_rgba(255,255,255,0.5),inset_1px_1px_3px_rgba(0,0,0,0.2)] duration-200"
+                    }`}
+                >
+                    {item.name}
+                </span>
+            </Link>
+        </ToolTipWrapper>
     );
 }
