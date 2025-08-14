08e7990bf2dd353210040b486949fff9e73c0653
AminFiroozi <afiroozi007@gmail.com>
2025-08-14 20:04:51 +0330

    fix: make admin-dashboard landing responsive

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -5,23 +5,34 @@ import React from "react";
 
 export default function Page() {
     return (
-        <div className="flex flex-col sm:flex-row relative bg-white justify-center h-full sm:h-fit">
-            <p className="vazir text-3xl sm:text-5xl flex m-auto w-2/3 text-center items-center sm:mt-0 sm:mr-4 leading-16">
+        <div className="flex flex-col sm:flex-row bg-white justify-center items-center h-full gap-4 p-6 text-center">
+            <p className="vazir text-3xl sm:text-5xl leading-16 text-center">
                 به سامانه مدیریت برق نو خوش آمدید!
             </p>
-            <div className="py-18">
-                <Image
-                    alt="panelGuy"
-                    // src={panelGuy1}
-                    src="/images/admin-dashboard/panelGuy3.jpg"
-                    // fill
-                    // style={{ objectFit: "cover" }}
-                    // unoptimized
-                    // style={{ width: "100%", height: "auto" }}
-                    width={1248}
-                    height={832}
-                />
-            </div>
+            <Image
+                alt="panelGuy"
+                src="/images/admin-dashboard/panelGuy3.jpg"
+                width={1248}
+                height={832}
+            />
         </div>
+        // <div className="flex flex-col sm:flex-row relative bg-white justify-center items-center h-full">
+        //     <p className="vazir text-3xl sm:text-5xl flex w-2/3 text-center items-end sm:items-center h-full sm:mr-4 leading-16">
+        //         به سامانه مدیریت برق نو خوش آمدید!
+        //     </p>
+        //     <div className="">
+        //         <Image
+        //             alt="panelGuy"
+        //             // src={panelGuy1}
+        //             src="/images/admin-dashboard/panelGuy3.jpg"
+        //             // fill
+        //             // style={{ objectFit: "cover" }}
+        //             // unoptimized
+        //             // style={{ width: "100%", height: "auto" }}
+        //             width={1248}
+        //             height={832}
+        //         />
+        //     </div>
+        // </div>
     );
 }
