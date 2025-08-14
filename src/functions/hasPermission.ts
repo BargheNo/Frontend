dd717f6fb1a7b3767de594efe1001508ec1d82e3
Amin Firoozi <114510343+AminFiroozi@users.noreply.github.com>
2025-08-14 14:17:07 +0330

    Fix/corp management (#224)
    * fix: use avatar for corp logo
    
    * fix: corp info displaying

--- src/functions/hasPermission.ts
+++ src/functions/hasPermission.ts
@@ -33,7 +33,7 @@ export default function useHasPermission(permission: string): PermissionState {
     const names = Array.isArray(permissions)
         ? permissions?.map((p: permission) => p.name)
         : [];
-    console.log(names);
+    // console.log(names);
     const hasPermission =
         isClient &&
         (names?.includes("general.all") || names?.includes(permission));
