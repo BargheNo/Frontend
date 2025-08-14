d34e95e3b7fe05bd182918181f4bac2964b995e4
AminFiroozi <afiroozi007@gmail.com>
2025-08-14 15:33:44 +0330

    fix: update panels list after making a new one

--- app/corpdashboard/installed-panels/page.tsx
+++ app/corpdashboard/installed-panels/page.tsx
@@ -1,22 +1,22 @@
-import AddPanel from "@/components/Add-Panel/add-panel";
+import AddPanel from "@/components/InstalledPanels/add-panel";
 import PageContainer from "@/components/Dashboard/PageContainer/PageContainer";
 import InstalledPanelPagination from "@/components/InstalledPanels/InstalldPanelPagination";
 import Head from "next/head";
 import React from "react";
 
 export default function page() {
-	return (
-		<>
-			<Head>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
-			</Head>
-			<PageContainer>
-				<AddPanel />
-				<InstalledPanelPagination />
-			</PageContainer>
-		</>
-	);
+    return (
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+            </Head>
+            <PageContainer>
+                {/* <AddPanel /> */}
+                <InstalledPanelPagination />
+            </PageContainer>
+        </>
+    );
 }
