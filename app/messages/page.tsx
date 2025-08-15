e8f001dc40be913a687a836459405aeb218b5fd4
Tina.2017 <tina.mpmp2017@gmail.com>
2025-08-15 23:22:12 +0330

    fix: added messages page to navbar

---
+++ app/messages/page.tsx
@@ -0,0 +1,18 @@
+import PageContainer from "@/components/Dashboard/PageContainer/PageContainer";
+import MessagesPagination from "@/components/Messages/message-pagination";
+import Head from "next/head";
+import React from "react";
+
+export default function page() {
+    return (
+        <PageContainer >
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+            </Head>
+            <MessagesPagination />
+        </PageContainer>
+    );
+}
