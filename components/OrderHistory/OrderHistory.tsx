e3d920b50298967411552803f60118b0e812bd18
AminFiroozi <afiroozi007@gmail.com>
2025-08-16 22:17:02 +0330

    feat: make address truncated

--- components/OrderHistory/OrderHistory.tsx
+++ components/OrderHistory/OrderHistory.tsx
@@ -5,8 +5,15 @@ import Link from "next/link";
 import SignupButton from "@/components/SignupButton/SignupButton";
 import { Orderhistory } from "@/src/types/OrderhistoryType";
 import moment from "jalali-moment";
+import TruncatedText from "../ui/TruncatedText";
 
-const OrderHistory = ({ name, id, status, address, createdTime }: Orderhistory) => {
+const OrderHistory = ({
+    name,
+    id,
+    status,
+    address,
+    createdTime,
+}: Orderhistory) => {
     return (
         <div
             className={` w-full ${
@@ -53,21 +60,28 @@ const OrderHistory = ({ name, id, status, address, createdTime }: Orderhistory) 
                         <div className="flex flex-row items-start text-black">
                             <IconWithBackground icon={MapPin} color="#6B7280" />
                             <div className="font-medium mx-2 place-self-center mt-2">
-                                استان {address.province}، شهر {address.city}،{" "}
-                                {address.streetAddress}، پلاک{" "}
-                                {address.houseNumber}، واحد {address.unit}
+                                <TruncatedText maxLength={135}>
+                                    {`
+                                    استان ${address.province}، شهر ${
+                                        address.city
+                                    }، ${address.streetAddress}، پلاک
+                                    ${address.houseNumber}، واحد ${String(
+                                        address.unit
+                                    )}
+                                    `}
+                                </TruncatedText>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="flex md:flex-col flex-row-reverse justify-center lg:-mb-0 -mb-80 gap-2 items-center z-10 min-w-48 -mr-43">
-                    <Link href={`new-order/${id}`} className="w-full px-4">
-                        <button className="w-full flex items-center justify-evenly gradient-blue px-4 py-2 text-white cursor-pointer shadow-md rounded-full hover:shadow-lg transition duration-300 hover:scale-105">
+                <div className="flex md:flex-col flex-row-reverse lg:-mb-0 -mb-80 gap-2 items-center z-10 min-w-48 -mr-43">
+                    <Link href={`new-order/${id}`} className="w-full p-8">
+                        <button className="w-full flex items-center gap-2 justify-evenly gradient-blue px-4 py-2 text-white cursor-pointer shadow-md rounded-full hover:shadow-lg transition duration-300 hover:scale-105">
                             <span className="font-medium text-nowrap">
                                 مدیریت سفارش
                             </span>
-                            <MoveLeft className="mr-2 w-4 h-4" />
+                            <MoveLeft className="min-w-4 h-4" />
                         </button>
                     </Link>
                     {/* <Link href="">
