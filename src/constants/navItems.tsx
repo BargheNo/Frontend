3c4c64adeece0d735b42e713489da5117bf39bb2
Pooya Tarashi <m.p.t.1383@gmail.com>
2025-08-23 01:35:39 +0330

    feat(panel-monitoring): remove unnecessary files

--- src/constants/navItems.tsx
+++ src/constants/navItems.tsx
@@ -62,11 +62,6 @@ export const CorpNavItems = [
         icon: <SquarePen />,
     },
     {
-        name: "پنل‌های من",
-        path: "/corpdashboard/my-panels",
-        icon: <Layers />,
-    },
-    {
         name: "پنل‌های نصب شده",
         path: "/corpdashboard/installed-panels",
         icon: <Server />,
