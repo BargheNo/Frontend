1ce91ed8618ab3046f7fff94ea7a838d26a5169c
Pooya Tarashi <m.p.t.1383@gmail.com>
2025-08-25 11:28:10 +0330

    hotfix

--- src/functions/hasPermission.ts
+++ src/functions/hasPermission.ts
@@ -33,10 +33,11 @@ export default function useHasPermission(permission: string): PermissionState {
     const names = Array.isArray(permissions)
         ? permissions?.map((p: permission) => p.name)
         : [];
-    // console.log(names);
+    console.log(names);
     const hasPermission =
         isClient &&
         (names?.includes("general.all") ||
+        names?.includes("corportion.all") ||
             names?.includes(permission) ||
             permission === "");
 
